a84ba62f78c11ccce48a515013c153a43206c6cc
David García García <deivid.garcia.garcia@gmail.com>
2016-11-22 09:23:25 +0100

    Issue #813902: Lock tests fail

--- src/SqlsrvServiceProvider.php
+++ src/SqlsrvServiceProvider.php
@@ -11,13 +11,21 @@ class SqlsrvServiceProvider implements ServiceProviderInterface {
    * {@inheritdoc}
    */
   public function register(ContainerBuilder $container) {
-    $definition = $container->getDefinition('lock');
-    if ($definition->getClass() == \Drupal\Core\Lock\DatabaseLockBackend::class) {
-      $definition->setClass(\Drupal\sqlsrv\Lock\DatabaseLockBackend::class);
-    }
-    $definition = $container->getDefinition('lock.persistent');
-    if ($definition->getClass() == \Drupal\Core\Lock\PersistentDatabaseLockBackend::class) {
-      $definition->setClass(\Drupal\sqlsrv\Lock\PersistentDatabaseLockBackend::class);
+    // Float columns are only broken in the PDO driver version prior to 4.0.0
+    // the actual version number differs from Linux/Windows as they keep different
+    // versioning...
+    // TODO: Remove this in the future, these lock overrides are not needed
+    // anymore after the fixes in the PDO driver.
+    // @see https://github.com/Microsoft/msphpsql/releases/tag/4.1.0
+    if (($version = phpversion("pdo_sqlsrv")) && version_compare($version, '4.0.0', '<')) {
+      $definition = $container->getDefinition('lock');
+      if ($definition->getClass() == \Drupal\Core\Lock\DatabaseLockBackend::class) {
+        $definition->setClass(\Drupal\sqlsrv\Lock\DatabaseLockBackend::class);
+      }
+      $definition = $container->getDefinition('lock.persistent');
+      if ($definition->getClass() == \Drupal\Core\Lock\PersistentDatabaseLockBackend::class) {
+        $definition->setClass(\Drupal\sqlsrv\Lock\PersistentDatabaseLockBackend::class);
+      }
     }
   }
 }
