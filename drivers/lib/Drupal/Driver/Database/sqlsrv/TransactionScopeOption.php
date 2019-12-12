467478c10e8088ed07a5fbe33456eea081ae98d8
Kevin Nowaczyk <beakerboy99@yahoo.com>
2019-12-12 13:49:16 -0500

    Update TransactionScopeOption.php

--- drivers/lib/Drupal/Driver/Database/sqlsrv/TransactionScopeOption.php
+++ drivers/lib/Drupal/Driver/Database/sqlsrv/TransactionScopeOption.php
@@ -3,7 +3,7 @@
 namespace Drupal\Driver\Database\sqlsrv;
 
 /**
- *
+ * This needs a description as to why it needs to be a class.
  */
 class TransactionScopeOption extends Enum {
   const RequiresNew = 'RequiresNew';
