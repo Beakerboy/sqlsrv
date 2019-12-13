754096c826e734e5ef36098da9a3c1ed358a6681
Kevin Nowaczyk <beakerboy99@yahoo.com>
2019-12-13 14:23:38 -0500

    Update bootstrap.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,7 +4,11 @@
  * Bootstrap File
  */
 if (file_exists(__DIR__ . '/../vendor/autoload.php')) {
+  echo "First Autoloader Found :-)";
   require_once __DIR__ . '/../vendor/autoload.php' ;
 } elseif (file_exists(__DIR__ . '/../../../autoload.php')) {
+    echo "Second Autoloader Found :-|";
     require_once __DIR__ . '/../../../autoload.php';
+} else {
+    echo "No Autoloader Found :-(";
 }
