b77f4bd9680905466eb1e5d2edb0c83d71dd2665
Kevin Nowaczyk <beakerboy99@yahoo.com>
2019-12-13 14:43:17 -0500

    removing debug outputs

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,11 +4,7 @@
  * Bootstrap File
  */
 if (file_exists(__DIR__ . '/../vendor/autoload.php')) {
-  echo "First Autoloader Found :-)";
   require_once __DIR__ . '/../vendor/autoload.php' ;
 } elseif (file_exists(__DIR__ . '/../../../autoload.php')) {
-    echo "Second Autoloader Found :-|";
     require_once __DIR__ . '/../../../autoload.php';
-} else {
-    echo "No Autoloader Found :-(";
 }
