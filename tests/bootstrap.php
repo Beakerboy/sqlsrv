a5bee538c3a776e2fbbd56eb8fd79cc15b71e61c
Kevin Nowaczyk <beakerboy99@yahoo.com>
2019-12-13 12:33:36 -0500

    altering autoload location

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,6 +3,8 @@
 /**
  * Bootstrap File
  */
-if (file_exists(__DIR__ . 'C:\projects\drupal-project\web\autoload.php')) {
-  require_once __DIR__ . 'C:\projects\drupal-project\web\autoload.php' ;
+if (file_exists(__DIR__ . '/../vendor/autoload.php')) {
+  require_once __DIR__ . '/../vendor/autoload.php' ;
+} elseif (file_exists(__DIR__ . '/../../../autoload.php')) {
+    require_once __DIR__ . '/../../../autoload.php';
 }
