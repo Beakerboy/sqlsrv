63f1f007d0157ac9d5697d432635aa5f81968e22
Kevin Nowaczyk <beakerboy99@yahoo.com>
2019-12-13 15:45:49 -0500

    adding Drupal test namespace manually

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,9 +3,6 @@
 /**
  * Bootstrap File
  */
-if (file_exists(__DIR__ . '/../vendor/autoload.php')) {
-  require_once __DIR__ . '/../vendor/autoload.php' ;
-} elseif (file_exists(__DIR__ . '/../../../autoload.php')) {
-    require_once __DIR__ . '/../../../autoload.php';
-}
-require_once __DIR__ . '/../vendor/drupal/core/tests/bootstrap.php' ;
+
+$loader = require __DIR__ . '../vendor/autoload.php' ;
+$loader->add('Drupal\\Tests', __DIR__ . '../vendor/drupal/core/tests');
