128a80264f741bf3209b02d13f2c501d25ea1a01
Kevin Nowaczyk <beakerboy99@yahoo.com>
2019-12-13 15:27:28 -0500

    adding drupal bootstrap file

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -8,3 +8,4 @@ if (file_exists(__DIR__ . '/../vendor/autoload.php')) {
 } elseif (file_exists(__DIR__ . '/../../../autoload.php')) {
     require_once __DIR__ . '/../../../autoload.php';
 }
+require_once __DIR__ . '/../vendor/drupal/core/tests/bootstrap.php' ;
