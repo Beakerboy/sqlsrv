d79f626aa9d123ab00c4f0d9d4b0562164dfd3d9
Kevin Nowaczyk <beakerboy99@yahoo.com>
2019-12-15 11:10:15 -0500

    Create File

---
+++ tests/src/Functional/DatabaseTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Drupal\Tests\sqlsrv\Functional;
+
+use Drupal\Tests\BrowserTestBase;
+
+/**
+ * Tests that the Rules UI pages are reachable.
+ *
+ * @group sqlsrv
+ */
+class DatabaseTest extends BrowserTestBase {
+
+}
