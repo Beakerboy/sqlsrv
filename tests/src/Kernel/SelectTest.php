e57209030768de3370f85923430716d29794fcfb
Kevin Nowaczyk <beakerboy99@yahoo.com>
2019-12-16 15:16:11 -0500

    Update SelectTest.php

--- tests/src/Kernel/SelectTest.php
+++ tests/src/Kernel/SelectTest.php
@@ -2,14 +2,15 @@
 
 namespace Drupal\KernelTests\sqlsrv\Database;
 
-use Drupal\KernelTests\Core\Database\SelectTest as CoreSelectTest;
+use Drupal\KernelTests\Core\Database\DatabaseTestBase;
 
 /**
  * Tests the select builder.
  *
  * @group Database
  */
-class SelectTest extends CoreSelectTest {
+class SelectTest extends DatabaseTestBase {
+  
   /**
    * The modules to load to run the test.
    *
@@ -19,4 +20,17 @@ class SelectTest extends CoreSelectTest {
     'sqlsrv',
     'database_test',
   ];
+
+  /**
+   * Tests rudimentary SELECT statements.
+   */
+  public function testSimpleSelect() {
+    $query = $this->connection->select('test');
+    $query->addField('test', 'name');
+    $query->addField('test', 'age', 'age');
+    $num_records = $query->countQuery()->execute()->fetchField();
+
+    $this->assertEqual($num_records, 4, 'Returned the correct number of rows.');
+  }
+
 }
