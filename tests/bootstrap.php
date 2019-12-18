daf58b9f0b4f27314f32226f5943ccabc42ce4a5
Kevin Nowaczyk <beakerboy99@yahoo.com>
2019-12-17 20:19:18 -0500

    Delete bootstrap.php

--- tests/bootstrap.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-/**
- * @file Bootstrap File
- */
-
-$loader = require __DIR__ . '/../vendor/autoload.php';
-$loader->add('Drupal\\Tests', __DIR__ . '/../vendor/drupal/core/tests');
-$loader->add('Drupal\\TestTools', __DIR__ . '/../vendor/drupal/core/tests');
