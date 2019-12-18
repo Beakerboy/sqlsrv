e01ea8442c5c19bfd6212df4a746c114f9beb17c
Kevin Nowaczyk <beakerboy99@yahoo.com>
2019-12-18 13:30:30 -0500

    Phpcs (#12)
    * turn on phpcs
    
    * Phpcs fixes
    
    * Delete bootstrap.php
    
    * Delete phpunit.xml

--- tests/bootstrap.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-/**
- * Bootstrap File
- */
-
-$loader = require __DIR__ . '/../vendor/autoload.php' ;
-$loader->add('Drupal\\Tests', __DIR__ . '/../vendor/drupal/core/tests');
-$loader->add('Drupal\\TestTools', __DIR__ . '/../vendor/drupal/core/tests');
