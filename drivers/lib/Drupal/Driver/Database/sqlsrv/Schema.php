3cdf8c95b70c62f593482944a29f4628cd93a07a
Kevin Nowaczyk <beakerboy99@yahoo.com>
2019-12-19 08:01:32 -0500

    quotes instead of bracket

--- drivers/lib/Drupal/Driver/Database/sqlsrv/Schema.php
+++ drivers/lib/Drupal/Driver/Database/sqlsrv/Schema.php
@@ -609,7 +609,7 @@ EOF
       . "INNER JOIN INFORMATION_SCHEMA.KEY_COLUMN_USAGE AS KU "
       . "ON TC.CONSTRAINT_TYPE = 'PRIMARY KEY' AND "
       . "TC.CONSTRAINT_NAME = KU.CONSTRAINT_NAME AND "
-      . "KU.table_name=[{$table_prefixed}] ORDER BY KU.ORDINAL_POSITION";
+      . "KU.table_name='{$table_prefixed}' ORDER BY KU.ORDINAL_POSITION";
     $result = $this->connection->query($query)->fetchAllAssoc('column_name');
     return array_keys($result);
   }
