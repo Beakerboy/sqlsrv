f4e3edd2dbd4ac5652e5485b0d691b83b937297a
Kevin Nowaczyk <beakerboy99@yahoo.com>
2019-12-31 16:34:50 -0500

    Make function just call a parent version

--- drivers/lib/Drupal/Driver/Database/sqlsrv/Condition.php
+++ drivers/lib/Drupal/Driver/Database/sqlsrv/Condition.php
@@ -21,13 +21,13 @@ class Condition extends QueryCondition {
 
     foreach ($conditions as &$condition) {
       if ($condition['operator'] == 'REGEXP') {
-        $condition['field'] = 'RegExCompiledMatch(' . $field;
-        $condition['operator'] = ',';
-        $condition['value'] = $condition['value'] . ') = 1';
+        //$condition['field'] = 'RegExCompiledMatch(' . $field;
+        //$condition['operator'] = ',';
+        //$condition['value'] = $condition['value'] . ') = 1';
       } else if ($condition['operator'] == 'NOT REGEXP') {
-        $condition['field'] = 'RegExCompiledMatch(' . $field;
-        $condition['operator'] = ',';
-        $condition['value'] = $condition['value'] . ') = 0';
+        //$condition['field'] = 'RegExCompiledMatch(' . $field;
+        //$condition['operator'] = ',';
+        //$condition['value'] = $condition['value'] . ') = 0';
       } 
     }
     parent::compile($connection, $queryPlaceholder);
