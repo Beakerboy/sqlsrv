3c14e654b95cfb0ca56a9baa67de81088d4155fe
Kevin Nowaczyk <beakerboy99@yahoo.com>
2019-12-31 14:51:10 -0500

    Create Condition.php

---
+++ drivers/lib/Drupal/Driver/Database/sqlsrv/Condition.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Drupal\Driver\Database\sqlsrv;
+
+use Drupal\Core\Database\Query\Condition as QueryCondition;
+use Drupal\Core\Database\Query\PlaceholderInterface;
+
+/**
+ * @addtogroup database
+ * @{
+ */
+class Condition extends QueryCondition {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function compile(Connection $connection, PlaceholderInterface $queryPlaceholder) {
+    // Find any REGEXP conditions and turn them into function calls
+    $conditions = &$this->conditions;
+
+    foreach ($conditions as &$condition) {
+      if ($condition['operator'] == 'REGEXP') {
+        $condition['field'] = 'RegExCompiledMatch(' . $field;
+        $condition['operator'] = ',';
+        $condition['value'] = $condition['value'] . ') = 1';
+      } else if ($condition['operator'] == 'NOT REGEXP') {
+        $condition['field'] = 'RegExCompiledMatch(' . $field;
+        $condition['operator'] = ',';
+        $condition['value'] = $condition['value'] . ') = 0';
+      }
+    }
+  }
+}
+
+/**
+ * @} End of "addtogroup database".
+ */
