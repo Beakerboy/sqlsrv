54ba1b447b54b1974280a8f089c741b8f7d09a63
Kevin Nowaczyk <beakerboy99@yahoo.com>
2020-01-01 18:51:22 -0500

    Update regex.cs

--- tests/regex.cs
+++ tests/regex.cs
@@ -5,15 +5,8 @@ using System.Text.RegularExpressions;
 public partial class RegExCompiled
 {
   [SqlFunction(IsDeterministic = true, IsPrecise = true)]
-  public static int RegExCompiledMatch(string pattern, string matchString)
+  public static bool RegExCompiledMatch(string pattern, string matchString)
   {
-    if (Regex.Match(matchString.TrimEnd(null), pattern.TrimEnd(null), RegexOptions.Compiled).Success)
-    {
-         return 1;
-    }
-    else
-    {
-         return 0;
-    }
+    return Regex.isMatch(matchString.TrimEnd(null), pattern.TrimEnd(null), RegexOptions.Compiled)
   }
 };
