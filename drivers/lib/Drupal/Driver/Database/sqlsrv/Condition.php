077ed8630d769d65044e17556fcbde181a43ff4a
Kevin Nowaczyk <beakerboy99@yahoo.com>
2020-01-02 07:51:56 -0500

    Using placeholder for the pattern

--- drivers/lib/Drupal/Driver/Database/sqlsrv/Condition.php
+++ drivers/lib/Drupal/Driver/Database/sqlsrv/Condition.php
@@ -20,14 +20,16 @@ class Condition extends QueryCondition {
     foreach ($this->conditions as &$condition) {
       if(isset($condition['operator'])) {
         $schema_name = $connection->schema->getDefaultSchema();
+        $placeholder = ':db_condition_placeholder_' . $queryPlaceholder->nextPlaceholder();
+        $field_fragment = $connection->escapeField($condition['field']);
         if ($condition['operator'] == 'REGEXP') {
-          $condition['field'] = '{$schema_name}.REGEXP(:db_regexp, ' . $connection->escapeField($condition['field']) . ') = 1';
+          $condition['field'] = '{$schema_name}.REGEXP({$placeholder}, {$field_fragment}) = 1';
           $condition['operator'] = NULL;
-          $condition['value'] = [':db_regexp' => $condition['value']];
+          $condition['value'] = [$placeholder => $condition['value']];
         } else if ($condition['operator'] == 'NOT REGEXP') {
-          $condition['field'] = '{$schema_name}.REGEXP(:db_regexp, ' . $connection->escapeField($condition['field']) . ') = 0';
+          $condition['field'] = '{$schema_name}.REGEXP({$placeholder}, {$field_fragment}) = 0';
           $condition['operator'] = NULL;
-          $condition['value'] = [':db_regexp' => $condition['value']];
+          $condition['value'] = [$placeholder => $condition['value']];
         } 
       }
     }
