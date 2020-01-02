5adb82ea720135c66b69b8b4bbc0d8ff0b0ecc2e
Kevin Nowaczyk <beakerboy99@yahoo.com>
2020-01-01 19:56:09 -0500

    Update regex.cs

--- tests/regex.cs
+++ tests/regex.cs
@@ -7,6 +7,6 @@ public partial class RegExCompiled
   [SqlFunction(IsDeterministic = true, IsPrecise = true)]
   public static bool RegExCompiledMatch(string pattern, string matchString)
   {
-    return Regex.isMatch(matchString.TrimEnd(null), pattern.TrimEnd(null), RegexOptions.Compiled);
+    return Regex.IsMatch(matchString.TrimEnd(null), pattern.TrimEnd(null), RegexOptions.Compiled);
   }
 };
