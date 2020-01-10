fc50fa44b8b686981411d13cdea0417afabe33aa
Kevin Nowaczyk <beakerboy99@yahoo.com>
2020-01-10 12:18:10 -0500

    missing paren

--- drivers/lib/Drupal/Driver/Database/sqlsrv/Schema.php
+++ drivers/lib/Drupal/Driver/Database/sqlsrv/Schema.php
@@ -1152,7 +1152,7 @@ EOF
     }
 
     // Recreate the unique constraint if it existed.
-    if ($unique_key && (!isset($new_keys['unique keys']) || !in_array($field_new, $new_keys['unique keys'])) {
+    if ($unique_key && (!isset($new_keys['unique keys']) || !in_array($field_new, $new_keys['unique keys']))) {
       $new_keys['unique keys'][] = $field_new;
     }
 
