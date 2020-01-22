5046d2bd758ee78ff5e8171329ae7aeb7128004a
Kevin Nowaczyk <beakerboy99@yahoo.com>
2020-01-22 08:17:29 -0500

    Add Custom Kernel Test

---
+++ tests/src/Kernel/QueryTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Drupal\Tests\sqlsrv\Kernel;
+
+use Drupal\KernelTests\Core\Database\DatabaseTestBase;
+
+/**
+ * Tests Drupal's extended prepared statement syntax..
+ *
+ * @group Database
+ */
+class QueryTest extends DatabaseTestBase {
+
+  /**
+   * Tests numeric query parameter expansion in expressions.
+   *
+   * @see \Drupal\Core\Database\Driver\sqlite\Statement::getStatement()
+   * @see http://bugs.php.net/bug.php?id=45259
+   */
+  public function testNumericExpressionSubstitution() {
+    $count = $this->connection->query('SELECT MAX(2, :count) FROM {test}', [
+      ':count' => 3,
+    ])->fetchField();
+    $this->assertEqual($count, 3);
+  }
+
+}
