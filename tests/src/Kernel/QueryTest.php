0c70719c9bb563bac1abb0efaac0af5e0ac5207a
Kevin Nowaczyk <beakerboy99@yahoo.com>
2020-01-22 12:21:40 -0500

    Replace unsupported query. (#32)
    * Add Custom Kernel
    
    * update PHPUnit.xml.dist

---
+++ tests/src/Kernel/QueryTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Drupal\Tests\sqlsrv\Kernel;
+
+use Drupal\KernelTests\Core\Database\QueryTest as DrupalQueryTest;
+
+/**
+ * Tests Drupal's extended prepared statement syntax..
+ *
+ * @group Database
+ */
+class QueryTest extends DrupalQueryTest {
+
+  /**
+   *{@inheritdoc)
+   */
+  public function testNumericExpressionSubstitution() {
+    $direct_count = $this->connection->query('SELECT count(*) + 3 FROM {test}');
+    $substituted_count = $this->connection->query('SELECT count(*) + :count FROM {test}', [
+      ':count' => 3,
+    ])->fetchField();
+    $this->assertEqual($direct_count, $substituted_count);
+  }
+
+}
