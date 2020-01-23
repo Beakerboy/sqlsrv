fafeea4dbf2222fb47635b7faca2d2f69c85ddf4
Kevin Nowaczyk <beakerboy99@yahoo.com>
2020-01-23 07:58:24 -0500

    Update Context.php

--- drivers/lib/Drupal/Driver/Database/sqlsrv/Context.php
+++ drivers/lib/Drupal/Driver/Database/sqlsrv/Context.php
@@ -5,7 +5,7 @@ namespace Drupal\Driver\Database\sqlsrv;
 use Drupal\Core\Database\Database;
 
 /**
- * The Context Class
+ * The Context Class.
  *
  * Defines a behaviour scope for the database driver that lasts until the object
  * is destroyed.
@@ -27,7 +27,7 @@ class Context {
   public $settings = NULL;
 
   /**
-   * Constructor
+   * Constructor.
    *
    * Define the behaviour of the database driver during the scope of the
    * life of this instance.
@@ -35,13 +35,10 @@ class Context {
    * @param Connection $connection
    *   Instance of the connection to be configured. Leave null to use the
    *   current default connection.
-   *
    * @param mixed $bypass_queries
    *   Do not preprocess the query before execution.
-   *
    * @param mixed $direct_query
    *   Prepare statements with SQLSRV_ATTR_DIRECT_QUERY = TRUE.
-   *
    * @param mixed $statement_caching
    *   Enable prepared statement caching. Cached statements are reused even
    *   after the context has expired.
