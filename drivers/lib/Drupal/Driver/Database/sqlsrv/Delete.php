a7d6d5ebbaee9ceb51e5b802a0d34fec26520f9a
Kevin Nowaczyk <beakerboy99@yahoo.com>
2020-01-23 08:58:49 -0500

    docblock

--- drivers/lib/Drupal/Driver/Database/sqlsrv/Delete.php
+++ drivers/lib/Drupal/Driver/Database/sqlsrv/Delete.php
@@ -5,6 +5,6 @@ namespace Drupal\Driver\Database\sqlsrv;
 use Drupal\Core\Database\Query\Delete as QueryDelete;
 
 /**
- *
+ * SQL Server implementation of \Drupal\Core\Database\Query\Delete.
  */
 class Delete extends QueryDelete {}
