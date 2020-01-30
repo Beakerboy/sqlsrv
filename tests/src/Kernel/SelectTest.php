5e9972a94ae1ac5f28aeb760a3d9719b29ea2e9a
Kevin Nowaczyk <beakerboy99@yahoo.com>
2020-01-29 20:33:46 -0500

    Update SelectTest.php

--- tests/src/Kernel/SelectTest.php
+++ tests/src/Kernel/SelectTest.php
@@ -4,7 +4,7 @@ namespace Drupal\Tests\sqlsrv\Kernel;
 use Drupal\Core\Database\InvalidQueryException;
 use Drupal\Core\Database\Database;
 
-use Drupal\Core\Database\Database\DatabaseTestBase;
+use Drupal\Core\Database\DatabaseTestBase;
 
 /**
  * Tests the Select query builder.
