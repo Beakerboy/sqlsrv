e77bd34657c47dc2cd69908096447b16763e33cf
Kevin Nowaczyk <beakerboy99@yahoo.com>
2020-01-30 13:10:05 -0500

    Update Schema.php

--- drivers/lib/Drupal/Driver/Database/sqlsrv/Schema.php
+++ drivers/lib/Drupal/Driver/Database/sqlsrv/Schema.php
@@ -316,7 +316,7 @@ class Schema extends DatabaseSchema {
   public function fieldSetDefault($table, $field, $default) {
     // phpcs:disable
     // Error format is necessary for kernel test, but does not meet the coding standard
-    @trigger_error('fieldSetDefault() is deprecated in Drupal 8.7.0 and will be removed before Drupal 9.0.0. Instead, call ::changeField() passing a full field specification. See https://www.drupal.org/node/2999035', E_USER_DEPRECATED);
+    @trigger_error('fieldSetDefault() is deprecated in drupal:8.7.0 and will be removed before drupal:9.0.0. Instead, call ::changeField() passing a full field specification. See https://www.drupal.org/node/2999035', E_USER_DEPRECATED);
     // phpcs:enable
     if (!$this->fieldExists($table, $field)) {
       throw new DatabaseSchemaObjectDoesNotExistException(t("Cannot set default value of field %table.%field: field doesn't exist.", ['%table' => $table, '%field' => $field]));
@@ -341,7 +341,7 @@ class Schema extends DatabaseSchema {
   public function fieldSetNoDefault($table, $field) {
     // phpcs:disable
     // Error format is necessary for kernel test, but does not meet the coding standard
-    @trigger_error('fieldSetNoDefault() is deprecated in Drupal 8.7.0 and will be removed before Drupal 9.0.0. Instead, call ::changeField() passing a full field specification. See https://www.drupal.org/node/2999035', E_USER_DEPRECATED);
+    @trigger_error('fieldSetNoDefault() is deprecated in drupal:8.7.0 and will be removed before drupal:9.0.0. Instead, call ::changeField() passing a full field specification. See https://www.drupal.org/node/2999035', E_USER_DEPRECATED);
     // phpcs:enable
     if (!$this->fieldExists($table, $field)) {
       throw new DatabaseSchemaObjectDoesNotExistException(t("Cannot remove default value of field %table.%field: field doesn't exist.", ['%table' => $table, '%field' => $field]));
