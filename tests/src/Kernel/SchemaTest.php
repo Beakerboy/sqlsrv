c0b0bff19c95829b92b75b72724bb809ab33e01f
Kevin Nowaczyk <beakerboy99@yahoo.com>
2020-01-31 00:51:29 -0500

    Update SchemaTest.php

--- tests/src/Kernel/SchemaTest.php
+++ tests/src/Kernel/SchemaTest.php
@@ -129,7 +129,7 @@ use SchemaIntrospectionTestTrait;
         ->condition('serial_column', $id)
         ->execute()
         ->fetchField();
-      $this->assertIdentical($field_value, $test_data, "ID: $id. Field: $field_value. Test: $test_data");
+      $this->assertIdentical($field_value, $test_data, "ID: $id. Field: ".strlen($field_value).'. Test: '.strlen($test_data));
     }
 
     // Check the field was changed.
