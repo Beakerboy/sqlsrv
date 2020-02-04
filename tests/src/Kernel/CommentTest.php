8d276a6b3f199d43c7cce4ae82d74f276f2aedb4
Kevin Nowaczyk <beakerboy99@yahoo.com>
2020-02-04 10:54:26 -0500

    Update CommentTest.php

--- tests/src/Kernel/CommentTest.php
+++ tests/src/Kernel/CommentTest.php
@@ -71,4 +71,24 @@ class CommentTest extends KernelTestBase {
     $comment = $this->connection->query($sql)->fetchField();
     $this->assertEquals('Test Comment', $comment);
   }
+
+   public function testGetCommentWithUnprefixedQuery() {
+    $pdo = new \PDO('sqlsrv:Server=localhost;Database=mydrupalsite', 'sa', 'Password12!');
+    $table = 'comment_table_test';
+    $prefixed_table = $this->connection->prefixTables('{'.$table.'}');
+    // Create table
+    $sql = "CREATE table $prefixed_table (comment_field_a int)";
+    $pdo->query($sql);
+    
+    // Add Comment
+    $sql = "EXEC sp_addextendedproperty @name=N'MS_Description', @value='Test Comment'";
+    $sql .= ",@level0type = N'Schema', @level0name = 'dbo'";
+    $sql .= ",@level1type = N'Table', @level1name = '{$prefixed_table}'";
+    $pdo->query($sql);
+
+    // Query Comment
+    $sql = "SELECT value FROM fn_listextendedproperty ('MS_Description','Schema','dbo','Table','{{table}}',NULL,NULL)";
+    $comment = $this->connection->query($sql)->fetchField();
+    $this->assertEquals('Test Comment', $comment);
+  }
 }
