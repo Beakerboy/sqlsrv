a99c71ed65d7ed321a91b29f9cd5d28b946a7ab9
Kevin Nowaczyk <beakerboy99@yahoo.com>
2020-02-04 10:27:39 -0500

    Update CommentTest.php

--- tests/src/Kernel/CommentTest.php
+++ tests/src/Kernel/CommentTest.php
@@ -30,13 +30,12 @@ class CommentTest extends KernelTestBase {
     $this->schema = $this->connection->schema();
   }
 
-  public function testGetCommentWithDriver() {
-    $table = 'comment_table_driver';
-    $prefixed_table = $this->connection->prefixTables('{'.$table.'}');
+  public function testGetCommentWithPrefixes() {
     $pdo = new \PDO('sqlsrv:Server=localhost;Database=mydrupalsite', 'sa', 'Password12!');
-
+    $table = 'comment_table_test';
+    $prefixed_table = $this->connection->prefixTables('{'.$table.'}');
     // Create table
-    $sql = 'CREATE table $prefixed_table (comment_field_a int)';
+    $sql = "CREATE table $prefixed_table (comment_field_a int)";
     $pdo->query($sql);
     
     // Add Comment
@@ -46,16 +45,10 @@ class CommentTest extends KernelTestBase {
     $pdo->query($sql);
 
     // Query Comment
-    $comment = $this->schema->getComment($table);
-    //$sql = "SELECT value FROM fn_listextendedproperty ('MS_Description','Schema','dbo','Table','comment_table_test',NULL,NULL)";
-     //        "SELECT value FROM fn_listextendedproperty ('MS_Description','Schema','dbo','Table','comment_table_test',NULL,NULL)";
-    //$statement = $pdo->query($sql);
-    //$results = $statement-> setFetchMode(\PDO::FETCH_NUM);
-    //$comment = $results->fetch();
     $sql = "SELECT value FROM fn_listextendedproperty ('MS_Description','Schema','dbo','Table','{$prefixed_table}',NULL,NULL)";
-   // $this->assertEquals('Test Comment', $comment);
-    $statement = $this->connection->query($sql);
-    $comment = $statement->fetchField();
-    $this->assertEquals('Test Comment', $comment);
+    $statement = $pdo->query($sql);
+    $statement->setFetchMode(\PDO::FETCH_NUM);
+    $comment = $statement->fetch();
+    $this->assertEquals('Test Comment', $comment[0]);
   }
 }
