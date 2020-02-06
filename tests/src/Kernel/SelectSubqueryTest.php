9a8bd7434896d270e709951921d703aed6b8dfb0
Kevin Nowaczyk <beakerboy99@yahoo.com>
2020-02-06 10:54:29 -0500

    Create SelectSubqueryTest.php

---
+++ tests/src/Kernel/SelectSubqueryTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Drupal\Tests\sqlsrv\Kernel;
+
+use Drupal\KernelTests\Core\Database\DatabaseTestBase;
+/**
+ * Tests the Select query builder.
+ *
+ * @group Database
+ */
+class SelectSubqueryTest extends DatabaseTestBase {
+
+  public function testIntegerAverage() {
+    // Create a subquery, which is just a normal query object.
+    $query = $this->connection->select('test', 't2');
+    $query->addExpression('AVG(t2.age)');
+    $average = $query->execute()->fetchField();
+    $this->assertEqual($average, '27');
+  }
