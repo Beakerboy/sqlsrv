9c6235bc77feefdc93034f23fc9ae146d68a99ea
Kevin Nowaczyk <beakerboy99@yahoo.com>
2020-02-13 08:12:35 -0500

    Update SelectTest.php

--- tests/src/Kernel/SelectTest.php
+++ tests/src/Kernel/SelectTest.php
@@ -9,4 +9,6 @@ class SelectTest extends KernelTestBase {
   // Test custom Condition class in where.
 
   // Test custom Condition class in having.
+
+  // Test custom Condition class on nested conditions.
 }
