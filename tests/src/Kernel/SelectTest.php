09bbb5bd37f0e90bb4a8042613f2fc432f5481f0
Kevin Nowaczyk <beakerboy99@yahoo.com>
2020-02-13 07:40:43 -0500

    Create SelectTest.php

---
+++ tests/src/Kernel/SelectTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Drupal\Tests\sqlsrv\Kernel;
+
+use Drupal\KernelTests\KernelTestBase;
+
+class SelectTest extends KernelTestBase {
+
+  // Test custom Condition class in where.
+
+  // Test custom Condition class in having.
+}
