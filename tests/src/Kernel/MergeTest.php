aca152b8d46a372aa461f2fd50d1a6466ec292d1
Kevin Nowaczyk <beakerboy99@yahoo.com>
2020-02-14 06:28:29 -0500

    Create MergeTest.php

---
+++ tests/src/Kernel/MergeTest.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Drupal\Tests\sqlsrv\Kernel;
+
+use Drupal\KernelTests\KernelTestBase;
+
+class MergeTest extends KernelTestBase {
+
+  // Test custom Condition class in merge.
+}
