1537e138d62b0275aaac5ec49cd6b4e5f678d28b
Kevin Nowaczyk <beakerboy99@yahoo.com>
2020-02-17 12:04:59 -0500

    Update ConditionTest.php

--- tests/src/Unit/ConditionTest.php
+++ tests/src/Unit/ConditionTest.php
@@ -1,11 +1,20 @@
 <?php
 
-namespace Drupal\Tests\sqlsrv\Unit
+namespace Drupal\Tests\sqlsrv\Unit;
+
+use Drupal\Driver\Database\sqlsrv\Select;
+use Drupal\Driver\Database\sqlsrv\Condition;
+use Drupal\Tests\Core\Database\Stub\StubConnection;
 
 /**
  * Test the behavior of the custom Condition class
  */
 class ConditionTest {
+  public function setUp() {
+    $mock_pdo = $this->createMock('Drupal\Tests\Core\Database\Stub\StubPDO');
+    $this->connection = new StubConnection($mock_pdo, []);
+    $this->select = new Select('table', 't', $this->connection);
+  }
 
   /**
    * Test the escaping strategy of the LIKE operator.
@@ -24,20 +33,23 @@ class ConditionTest {
    */
   public function testLike($given, $reescaped) {
    
-    $condition = [
-      'field' => 'name',
-      'value' = $given,
-      'operation' => 'LIKE',
-    ];
-
+    $condition = new Condition('AND');
+    $condition->condition('name', $given, 'LIKE');
+    $condition->compile($this->connection, $select);
+    $conditions = $condition->conditions();
+    $reescaped = $conditions[0]['value'];
     $this->assertEqual($reescaped, $expected, "Test that the driver escapes LIKE parameters correctly");
   }
 
   public function dataProviderForTestLike() {
     return [
       [
+        '%',
+        '%',
       ],
       [
+        '\%',
+        '[%]',
       ],
     ];
   }
@@ -45,8 +57,8 @@ class ConditionTest {
   /**
    * Test the REGEXP operator string replacement
    */
-  public function testRegexp() {
+  //public function testRegexp() {
   
-  }
+  //}
 
 }
