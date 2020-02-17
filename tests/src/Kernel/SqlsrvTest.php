6a0a82b3b2de3b4f07fe829e9a613f1e0b95f73a
Kevin Nowaczyk <beakerboy99@yahoo.com>
2020-02-17 14:50:50 -0500

    Update SqlsrvTest.php

--- tests/src/Kernel/SqlsrvTest.php
+++ tests/src/Kernel/SqlsrvTest.php
@@ -56,32 +56,6 @@ class SqlsrvTest extends DatabaseTestBase {
   }
 
   /**
-   * Test for weird key names
-   * in array arguments.
-   */
-  public function testBadKeysInArrayArguments() {
-    $params[':nids'] = array(
-        'uid1' => -9,
-        'What a bad placeholder name, why should we care?' => -6,
-        );
-    $result = NULL;
-    try {
-      // The regular expandArguments implementation will fail to
-      // properly expand the associative array with weird keys, OH, and actually
-      // you can perform some SQL Injection through the array keys.
-      $result = db_query('SELECT COUNT(*) FROM users WHERE users.uid IN (:nids)', $params)->execute()->fetchField();
-    } 
-    catch (\Exception $err) {
-      // Regular drupal will fail with
-      // SQLSTATE[IMSSP]: An error occurred substituting the named parameters.
-      // https://www.drupal.org/node/2146839
-    }
-
-    // User ID's are negative, so this should return 0 matches.
-    $this->assertEqual($result, 0, 'Returned the correct number of total rows.');
-  }
-
-  /**
    * Test the temporary table functionality.
    */
   public function testTemporaryTables() {
