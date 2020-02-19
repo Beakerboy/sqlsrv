3d8051955ffd95e65e5cf5fd612e761c6eb0afdd
Kevin Nowaczyk <beakerboy99@yahoo.com>
2020-02-19 12:11:49 -0500

    Update ParamaterTest.php

--- tests/src/Kernel/ParamaterTest.php
+++ tests/src/Kernel/ParamaterTest.php
@@ -4,6 +4,9 @@ namespace Drupal\Tests\sqlsrv\Kernel;
 
 use Drupal\KernelTests\Core\Database\DatabaseTestBase;
 
+/**
+ * Tests parameter behavior.
+ */
 class ParameterTest extends DatabaseTestBase {
 
   /**
@@ -12,17 +15,17 @@ class ParameterTest extends DatabaseTestBase {
    * Remove any custom code related to this issue, but keep the test.
    */
   public function testBadKeysInArrayArguments() {
-    $params[':nids'] = array(
-        'uid1' => -9,
-        'What a bad placeholder name, why should we care?' => -6,
-        );
+    $params[':nids'] = [
+      'uid1' => -9,
+      'What a bad placeholder name, why should we care?' => -6,
+    ];
     $result = NULL;
     try {
       // The regular expandArguments implementation will fail to
       // properly expand the associative array with weird keys, OH, and actually
       // you can perform some SQL Injection through the array keys.
       $result = db_query('SELECT COUNT(*) FROM users WHERE users.uid IN (:nids)', $params)->execute()->fetchField();
-    } 
+    }
     catch (\Exception $err) {
       // Regular drupal will fail with
       // SQLSTATE[IMSSP]: An error occurred substituting the named parameters.
