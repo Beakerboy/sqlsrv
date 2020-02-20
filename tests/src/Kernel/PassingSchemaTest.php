2a32c40b7499b5da9fdb27fc8afaa1f9a82614c1
Kevin Nowaczyk <beakerboy99@yahoo.com>
2020-02-20 15:39:28 -0500

    phpcs

--- tests/src/Kernel/PassingSchemaTest.php
+++ tests/src/Kernel/PassingSchemaTest.php
@@ -5,7 +5,7 @@ namespace Drupal\Tests\sqlsrv\Kernel;
 use Drupal\KernelTests\Core\Database\SchemaTest;
 
 /**
- * Passing tests from SchemaTest.php
+ * Passing tests from SchemaTest.php.
  *
  * There are failing tests within the SchemaTest Kernel test.
  * This class separates the passing tests from the failing tests
@@ -21,6 +21,8 @@ class PassingSchemaTest extends SchemaTest {
   }
 
   /**
+   * {@inheritdoc}
+   *
    * @dataProvider providerTestSchemaCreateTablePrimaryKey
    */
   public function testSchemaChangePrimaryKey(array $initial_primary_key, array $renamed_primary_key) {
@@ -28,16 +30,40 @@ class PassingSchemaTest extends SchemaTest {
   }
 
   /**
-   * Change default value with numeric values
+   * Change default value with numeric values.
    */
   public function testSchemaChangeFieldDefaultInitialNumeric() {
     $field_specs = [
       ['type' => 'int', 'size' => 'normal', 'not null' => FALSE],
-      ['type' => 'int', 'size' => 'normal', 'not null' => TRUE, 'initial' => 1, 'default' => 17],
+      [
+        'type' => 'int',
+        'size' => 'normal',
+        'not null' => TRUE,
+        'initial' => 1,
+        'default' => 17,
+      ],
       ['type' => 'float', 'size' => 'normal', 'not null' => FALSE],
-      ['type' => 'float', 'size' => 'normal', 'not null' => TRUE, 'initial' => 1, 'default' => 7.3],
-      ['type' => 'numeric', 'scale' => 2, 'precision' => 10, 'not null' => FALSE],
-      ['type' => 'numeric', 'scale' => 2, 'precision' => 10, 'not null' => TRUE, 'initial' => 1, 'default' => 7],
+      [
+        'type' => 'float',
+        'size' => 'normal',
+        'not null' => TRUE,
+        'initial' => 1,
+        'default' => 7.3,
+      ],
+      [
+        'type' => 'numeric',
+        'scale' => 2,
+        'precision' => 10,
+        'not null' => FALSE,
+      ],
+      [
+        'type' => 'numeric',
+        'scale' => 2,
+        'precision' => 10,
+        'not null' => TRUE,
+        'initial' => 1,
+        'default' => 7,
+      ],
     ];
     foreach ($field_specs as $i => $old_spec) {
       foreach ($field_specs as $j => $new_spec) {
@@ -51,17 +77,20 @@ class PassingSchemaTest extends SchemaTest {
   }
 
   /**
-   * {@inhertidoc}
+   * {@inheritdoc}
    *
    * @dataProvider dataProviderForDefaultInitial
    */
-  public function testSchemaChangeFieldDefaultInitial($old_spec=[], $new_spec=[]) {
+  public function testSchemaChangeFieldDefaultInitial($old_spec = [], $new_spec = []) {
     // Note if the serialized data contained an object this would fail on
     // Postgres.
     // @see https://www.drupal.org/node/1031122
     $this->assertFieldChange($old_spec, $new_spec, serialize(['string' => "This \n has \\\\ some backslash \"*string action.\\n"]));
   }
 
+  /**
+   * Data Provider.
+   */
   public function dataProviderForDefaultInitial() {
     $varchar_ascii = ['type' => 'varchar_ascii', 'length' => '255'];
     $varchar = ['type' => 'varchar', 'length' => '255'];
