b9f62e5763936e645fa49377c483b65dbbb05c56
Kevin Nowaczyk <beakerboy99@yahoo.com>
2020-02-20 15:50:25 -0500

    phpcs

--- tests/src/Kernel/SqlsrvTest.php
+++ tests/src/Kernel/SqlsrvTest.php
@@ -4,6 +4,7 @@ namespace Drupal\Tests\sqlsrv\Kernel;
 
 use Drupal\KernelTests\Core\Database\DatabaseTestBase;
 use Drupal\Driver\Database\sqlsrv\Condition;
+
 /**
  * Test behavior that is unique to the Sql Server Driver.
  *
@@ -16,7 +17,7 @@ class SqlsrvTest extends DatabaseTestBase {
    */
   public function testParameterLimit() {
     $values = [];
-    for ($x = 0; $x < 2200; $x ++) {
+    for ($x = 0; $x < 2200; $x++) {
       $values[] = uniqid($x, TRUE);
     }
     $query = $this->connection->select('test_task', 't');
@@ -26,18 +27,19 @@ class SqlsrvTest extends DatabaseTestBase {
     // If > 2100 we can get SQL Exception! The driver must handle that.
     try {
       $result = $query->execute()->fetchField();
-    
-    } catch (\Exception $err)
-    {
     }
-    
+    catch (\Exception $err) {
+    }
+
     $this->assertEqual($result, 0, 'Returned the correct number of total rows.');
   }
 
   /**
+   *  Test duplicate placeholders in queries.
+   *
    * Although per official documentation you cannot send
    * duplicate placeholders in same query, this works in mySQL
-   * and is present in some queries, even in core, wich have not
+   * and is present in some queries, even in core, which have not
    * gotten enough attention.
    */
   public function testDuplicatePlaceholders() {
@@ -48,10 +50,10 @@ class SqlsrvTest extends DatabaseTestBase {
     // If > 2100 we can get SQL Exception! The driver must handle that.
     try {
       $result = $query->execute()->fetchField();
-    } 
+    }
     catch (\Exception $err) {
     }
-    
+
     $this->assertEqual($result, 2, 'Returned the correct number of total rows.');
   }
 
@@ -59,38 +61,37 @@ class SqlsrvTest extends DatabaseTestBase {
    * Test the temporary table functionality.
    */
   public function testTemporaryTables() {
-    
+
     $query = $this->connection->select('test_task', 't');
     $query->fields('t');
-    
+
     $table = $this->connection->queryTemporary((string) $query);
-    
-    // First assert that the table exists
+
+    // First assert that the table exists.
     $this->assertTRUE(db_table_exists($table), 'The temporary table exists.');
-    
+
     $query2 = $this->connection->select($table, 't');
     $query2->fields('t');
-    
+
     // Now make sure that both tables are exactly the same.
     $data1 = $query->execute()->fetchAllAssoc('tid');
     $data2 = $query2->execute()->fetchAllAssoc('tid');
 
     // User ID's are negative, so this should return 0 matches.
     $this->assertEqual(count($data1), count($data2), 'Temporary table has the same number of rows.');
-    // $this->assertEqual(count($data1[0]), count($data2[0]), 'Temporary table has the same number of columns.');
-    
+
     // Drop the table.
     $this->connection->schema()->dropTable($table);
-    
+
     // The table should not exist now.
     $this->assertFALSE($this->connection->schema()->tableExists($table), 'The temporary table does not exists.');
   }
-  
+
   /**
    * Test LIKE statement wildcards are properly escaped.
    */
   public function testEscapeLike() {
-    // Test expected escaped characters
+    // Test expected escaped characters.
     $string = 't[e%s]t_\\';
     $escaped_string = $this->connection->escapeLike($string);
     $this->assertEqual($escaped_string, 't[e\%s]t\_\\\\', 'Properly escaped string with backslashes');
@@ -100,7 +101,7 @@ class SqlsrvTest extends DatabaseTestBase {
     $condition->compile($this->connection, $query);
     $arguments = $condition->conditions();
     $argument = $arguments[0];
-    
+
     $expected = 't[[]e[%]s[]]t[_]\\';
     $actual = $argument['value'];
     $this->assertEqual($actual, $expected, 'Properly escaped LIKE statement wildcards.');
@@ -125,7 +126,8 @@ class SqlsrvTest extends DatabaseTestBase {
     $result = $query->execute()->fetchAll();
     $this->assertEqual(count($result), 1, t('db_select returned the correct number of total rows.'));
 
-    // Using the condition function required that only % and _ can be used as wildcards
+    // Using the condition function requires that only % and _ can be used as
+    // wildcards.
     // select->condition: Test unescaped wildcard.
     $query = $this->connection->select('test_task', 't');
     $query->condition('t.task', '_leep', 'LIKE');
@@ -141,8 +143,8 @@ class SqlsrvTest extends DatabaseTestBase {
     $this->assertEqual(count($result), 0, t('db_select returned the correct number of total rows.'));
 
     // Using the where function requires that database-specific notation be used.
-    // This means we can use the SQL Server bracket notation, but these queries will not be
-    // valid on other databases.
+    // This means we can use the SQL Server bracket notation, but these queries
+    // will not be valid on other databases.
     // select->where: Test unescaped wildcard.
     $query = $this->connection->select('test_task', 't');
     $query->where('t.task LIKE :task', [':task' => '[s]leep']);
@@ -157,8 +159,8 @@ class SqlsrvTest extends DatabaseTestBase {
     $result = $query->execute()->fetchAll();
     $this->assertEqual(count($result), 0, t('db_select returned the correct number of total rows.'));
 
-    // Using a static query also allows us to use database-specific syntax. Again, queries
-    // may not run on other databases.
+    // Using a static query also allows us to use database-specific syntax.
+    // Again, queries may not run on other databases.
     // query: Test unescaped wildcard.
     $query = $this->connection->query('SELECT COUNT(*) FROM {test_task} WHERE task LIKE :task',
       [':task' => '[s]leep']);
