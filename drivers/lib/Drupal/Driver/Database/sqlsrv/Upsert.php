0061a00e7cd3b8f2e30da09d5c74034d0c0c8134
Kevin Nowaczyk <beakerboy99@yahoo.com>
2020-03-25 13:02:21 -0400

    Replace upsert

--- drivers/lib/Drupal/Driver/Database/sqlsrv/Upsert.php
+++ drivers/lib/Drupal/Driver/Database/sqlsrv/Upsert.php
@@ -5,10 +5,7 @@ namespace Drupal\Driver\Database\sqlsrv;
 use Drupal\Core\Database\Query\Upsert as QueryUpsert;
 
 /**
- * Implements the Upsert query for the MSSQL database driver.
- *
- * TODO: This class has been replaced by UpsertNative.
- * Keeping this here for a while though..
+ * Implements Native Upsert queries for MSSQL.
  */
 class Upsert extends QueryUpsert {
 
@@ -19,91 +16,34 @@ class Upsert extends QueryUpsert {
     if (!$this->preExecute()) {
       return NULL;
     }
-
-    // Default options for upsert queries.
-    $this->queryOptions += [
-      'throw_exception' => TRUE,
-    ];
-
-    // Default fields are always placed first for consistency.
     $insert_fields = array_merge($this->defaultFields, $this->insertFields);
-    $insert_fields_escaped = array_map(function ($f) {
-      return $this->connection->escapeField($f);
-    }, $insert_fields);
-
-    $table = $this->connection->escapeTable($this->table);
-    $unique_key = $this->connection->escapeField($this->key);
-
-    // We have to execute multiple queries, therefore we wrap everything in a
-    // transaction so that it is atomic where possible.
+    // Start transaction
     $transaction = $this->connection->startTransaction();
-
-    // First, create a temporary table with the same schema as the table we
-    // are trying to upsert in.
-    $query = 'SELECT TOP(0) * FROM {' . $table . '}';
-    $temp_table = $this->connection->queryTemporary($query, [], array_merge($this->queryOptions, ['real_table' => TRUE]));
-
-    // Second, insert the data in the temporary table.
-    $insert = $this->connection->insert($temp_table, $this->queryOptions)
-      ->fields($insert_fields);
     foreach ($this->insertValues as $insert_values) {
-      $insert->values($insert_values);
-    }
-    $insert->execute();
-
-    // Third, lock the table we're upserting into.
-    $this->connection->query("SELECT 1 FROM {{$table}} WITH (HOLDLOCK)", [], $this->queryOptions);
-
-    // Fourth, update any rows that can be updated. This results in the
-    // following query:
-    //
-    // UPDATE table_name
-    // SET column1 = temp_table.column1 [, column2 = temp_table.column2, ...]
-    // FROM temp_table
-    // WHERE table_name.id = temp_table.id;.
-    $update = [];
-    foreach ($insert_fields_escaped as $field) {
-      if ($field !== $unique_key) {
-        $update[] = "$field = {" . $temp_table . "}.$field";
+      $update_fields = array_combine($insert_fields, $insert_values);
+      $condition = $update_fields[$this->key];
+      unset($update_fields[$this->key]);
+      // UPDATE WHERE key=value
+      $update = $this->connection->update($this->table, $this->queryOptions)
+        ->fields($update_fields)
+        ->condition($this->key, $condition);
+      $number = $update->execute();
+    
+      if ($number === 0) {
+       // INSERT table (fields) SELECT values WHERE ROWCOUNT=0
+        $insert = $this->connection->insert($this->table, $this->queryOptions)
+          ->fields($insert_fields)
+          ->values($insert_values)
+          ->execute();
       }
     }
-
-    $update_query = 'UPDATE {' . $table . '} SET ' . implode(', ', $update);
-    $update_query .= ' FROM {' . $temp_table . '}';
-    $update_query .= ' WHERE {' . $temp_table . '}.' . $unique_key . ' = {' . $table . '}.' . $unique_key;
-    $this->connection->query($update_query, [], $this->queryOptions);
-
-    // Fifth, insert the remaining rows. This results in the following query:
-    //
-    // INSERT INTO table_name
-    // SELECT
-    // temp_table.primary_key, temp_table.column1 [, temp_table.column2 ...]
-    // FROM temp_table
-    // LEFT OUTER JOIN table_name ON (table_name.id = temp_table.id)
-    // WHERE table_name.id IS NULL;.
-    $select = $this->connection->select($temp_table, 'temp_table', $this->queryOptions)
-      ->fields('temp_table', $insert_fields);
-    $select->leftJoin($this->table, 'actual_table', 'actual_table.' . $this->key . ' = temp_table.' . $this->key);
-    $select->isNull('actual_table.' . $this->key);
-
-    $this->connection->insert($this->table, $this->queryOptions)
-      ->from($select)
-      ->execute();
-
-    // Drop the "temporary" table.
-    $this->connection->query_direct("DROP TABLE {$temp_table}");
-
-    // Re-initialize the values array so that we can re-use this query.
-    $this->insertValues = [];
-
-    return TRUE;
   }
 
   /**
    * {@inheritdoc}
    */
   public function __toString() {
-    // Nothing to do.
+    // Do nothing.
   }
 
 }
