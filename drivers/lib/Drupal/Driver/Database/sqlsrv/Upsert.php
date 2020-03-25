4dc6689ae6abcbd46c6eb2fd6b19c67d8b9c616b
Kevin Nowaczyk <beakerboy99@yahoo.com>
2020-03-25 13:56:33 -0400

    [skip ci]

--- drivers/lib/Drupal/Driver/Database/sqlsrv/Upsert.php
+++ drivers/lib/Drupal/Driver/Database/sqlsrv/Upsert.php
@@ -17,20 +17,19 @@ class Upsert extends QueryUpsert {
       return NULL;
     }
     $insert_fields = array_merge($this->defaultFields, $this->insertFields);
-    // Start transaction
+    // Start transaction.
     $transaction = $this->connection->startTransaction();
     foreach ($this->insertValues as $insert_values) {
       $update_fields = array_combine($insert_fields, $insert_values);
       $condition = $update_fields[$this->key];
       unset($update_fields[$this->key]);
-      // UPDATE WHERE key=value
+
       $update = $this->connection->update($this->table, $this->queryOptions)
         ->fields($update_fields)
         ->condition($this->key, $condition);
       $number = $update->execute();
-    
+
       if ($number === 0) {
-       // INSERT table (fields) SELECT values WHERE ROWCOUNT=0
         $insert = $this->connection->insert($this->table, $this->queryOptions)
           ->fields($insert_fields)
           ->values($insert_values)
