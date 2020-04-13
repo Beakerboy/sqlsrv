afc89e46b8621b9e19d7441706b1ab5d832d76dc
Kevin Nowaczyk <beakerboy99@yahoo.com>
2020-04-13 10:36:59 -0400

    Core testsuite (#110)
    * Add test suite [skip ci]
    
    * Add testBase [skip ci]
    
    * add suite to xml [skip ci]
    
    * Add suite test to travis [skip appveyor]
    
    * Update CoreKernelTestSuite.php
    
    * Update CoreKernelTestSuite.php
    
    * [skip ci]
    
    * Phpcs and phpstan

---
+++ tests/src/TestSuites/CoreKernelTestSuite.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Drupal\Tests\sqlsrv\TestSuites;
+
+use Drupal\Core\Test\TestDiscovery;
+
+require_once __DIR__ . '/TestSuiteBase.php';
+
+/**
+ * Discovers tests for the kernel test suite.
+ */
+final class CoreKernelTestSuite extends TestSuiteBase {
+
+  /**
+   * Factory method which loads up a suite with all core kernel tests.
+   *
+   * @return static
+   *   The test suite.
+   */
+  public static function suite() {
+    $root = dirname(dirname(dirname(dirname(dirname(dirname(__DIR__))))));
+    $suite = new static('kernel');
+    $suite->addCoreKernelTests($root);
+    return $suite;
+  }
+
+  /**
+   * Find and add tests to the suite for core and any extensions.
+   *
+   * @param string $root
+   *   Path to the root of the Drupal installation.
+   */
+  protected function addCoreKernelTests($root) {
+    // Core's Kernel tests are in the namespace Drupal\KernelTests\ and are
+    // always inside of core/tests/Drupal/KernelTests.
+    $this->addTestFiles(TestDiscovery::scanDirectory("Drupal\\KernelTests\\", "$root/core/tests/Drupal/KernelTests"));
+  }
+
+}
