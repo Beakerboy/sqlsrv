eb5b3dd210927413866ca5e2856802c8a99bfd90
Kevin Nowaczyk <beakerboy99@yahoo.com>
2020-04-13 13:25:58 -0400

    [skip ci]

--- tests/src/TestSuites/CoreExtensions1KernelTestSuite.php
+++ tests/src/TestSuites/CoreExtensions1KernelTestSuite.php
@@ -9,7 +9,7 @@ require_once __DIR__ . '/TestSuiteBase.php';
 /**
  * Discovers tests for the kernel test suite.
  */
-final class CoreKernelExtensionAHTestSuite extends TestSuiteBase {
+final class CoreExtensions1KernelTestSuite extends TestSuiteBase {
 
   /**
    * Factory method which loads up a suite with all kernel tests.
