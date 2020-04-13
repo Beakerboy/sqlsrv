799150078a7be50371790b000f4cf214ded5c986
Kevin Nowaczyk <beakerboy99@yahoo.com>
2020-04-13 10:57:32 -0400

    Create CoreKernelExtensionAHTestSuite.php

---
+++ tests/src/TestSuites/CoreKernelExtensionAHTestSuite.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Drupal\Tests\sqlsrv\TestSuites;
+
+require_once __DIR__ . '/TestSuiteBase.php';
+
+/**
+ * Discovers tests for the kernel test suite.
+ */
+final class CoreKernelExtensionAHTestSuite extends TestSuiteBase {
+
+  /**
+   * Factory method which loads up a suite with all kernel tests.
+   *
+   * @return static
+   *   The test suite.
+   */
+  public static function suite() {
+    $root = dirname(dirname(dirname(dirname(dirname(dirname(__DIR__))))));
+    $suite = new static('kernel');
+    $suite->addExtensionTestsBySuiteNamespace($root, 'Kernel', '[A-Ha-h]');
+    return $suite;
+  }
+
+  /**
+   * Find and add tests to the suite for core and any extensions.
+   *
+   * @param string $root
+   *   Path to the root of the Drupal installation.
+   * @param string $suite_namespace
+   *   SubNamespace used to separate test suite. Examples: Unit, Functional.
+   * @param string $pattern
+   *   REGEXP pattern to apply to file name.
+   */
+  protected function addExtensionTestsBySuiteNamespace($root, $suite_namespace, $pattern) {
+    // Extensions' tests will always be in the namespace
+    // Drupal\Tests\$extension_name\$suite_namespace\ and be in the
+    // $extension_path/tests/src/$suite_namespace directory. Not all extensions
+    // will have all kinds of tests.
+    foreach ($this->findExtensionDirectories($root) as $extension_name => $dir) {
+      if (preg_match("#^{$pattern}(.*)$#i", $extension_name) !== 0) {
+        $test_path = "$dir/tests/src/$suite_namespace";
+        if (is_dir($test_path)) {
+          $this->addTestFiles(TestDiscovery::scanDirectory("Drupal\\Tests\\$extension_name\\$suite_namespace\\", $test_path));
+        }
+      }
+    }
+  }
+
+}
