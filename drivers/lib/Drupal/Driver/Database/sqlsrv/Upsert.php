48c74fa8186b8f4956c44f8d55676815f9248a59
Kevin Nowaczyk <beakerboy99@yahoo.com>
2020-04-16 09:13:39 -0400

    Update Upsert.php

--- drivers/lib/Drupal/Driver/Database/sqlsrv/Upsert.php
+++ drivers/lib/Drupal/Driver/Database/sqlsrv/Upsert.php
@@ -81,7 +81,7 @@ class Upsert extends QueryUpsert {
     $update_string = 'UPDATE SET ' . $update_list;
     $insert_string = 'INSERT ' . $field_list . ' VALUES ' . $insert_list;
     $query = 'MERGE {' . $this->table . '} AS t USING(' . $values_string . ')';
-    $query .= ' src ' . $field_list . ' ON t.' . $key . '=src.'. $key;
+    $query .= ' AS src ' . $field_list . ' ON t.' . $key . '=src.'. $key;
     $query .= ' WHEN MATCHED THEN ' . $update_string;
     $query .= ' WHEN NOT MATCHED THEN ' . $insert_string;
 
