0574456130d50bfa92f8a9121260c32419552000
Kevin Nowaczyk <beakerboy99@yahoo.com>
2020-04-17 19:14:58 -0400

    Update CoreExtensionsUnitTestSuite.php

--- tests/src/TestSuites/CoreExtensionsUnitTestSuite.php
+++ tests/src/TestSuites/CoreExtensionsUnitTestSuite.php
@@ -31,8 +31,6 @@ final class CoreExtensionsUnitTestSuite extends TestSuiteBase {
    *   Path to the root of the Drupal installation.
    * @param string $suite_namespace
    *   SubNamespace used to separate test suite. Examples: Unit, Functional.
-   * @param string $pattern
-   *   REGEXP pattern to apply to file name.
    */
   protected function addExtensionTestsBySuiteNamespace($root, $suite_namespace) {
     foreach ($this->findExtensionDirectories($root) as $extension_name => $dir) {
