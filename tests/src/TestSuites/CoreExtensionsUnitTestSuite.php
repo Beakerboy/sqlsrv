1e79a6553e3075c190ce8c3b824c778bf81cf304
Kevin Nowaczyk <beakerboy99@yahoo.com>
2020-04-18 08:58:57 -0400

    Unit extensions (#116)

---
+++ tests/src/TestSuites/CoreExtensionsUnitTestSuite.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Drupal\Tests\sqlsrv\TestSuites;
+
+use Drupal\Core\Test\TestDiscovery;
+
+require_once __DIR__ . '/TestSuiteBase.php';
+
+/**
+ * Discovers tests for the kernel test suite.
+ */
+final class CoreExtensionsUnitTestSuite extends TestSuiteBase {
+
+  /**
+   * Factory method which loads up a suite with all kernel tests.
+   *
+   * @return static
+   *   The test suite.
+   */
+  public static function suite() {
+    $root = dirname(dirname(dirname(dirname(dirname(dirname(__DIR__))))));
+    $suite = new static('unit');
+    $suite->addExtensionTestsBySuiteNamespace($root, 'Unit');
+    return $suite;
+  }
+
+  /**
+   * Find and add tests to the suite for core and any extensions.
+   *
+   * @param string $root
+   *   Path to the root of the Drupal installation.
+   * @param string $suite_namespace
+   *   SubNamespace used to separate test suite. Examples: Unit, Functional.
+   */
+  protected function addExtensionTestsBySuiteNamespace($root, $suite_namespace) {
+    foreach ($this->findExtensionDirectories($root) as $extension_name => $dir) {
+      $test_path = "$dir/tests/src/$suite_namespace";
+      if (is_dir($test_path)) {
+        $this->addTestFiles(TestDiscovery::scanDirectory("Drupal\\Tests\\$extension_name\\$suite_namespace\\", $test_path));
+      }
+    }
+  }
+
+}
