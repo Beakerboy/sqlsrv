6c68ed9e523305d19e200527dad6d9e9ce4161cc
Kevin Nowaczyk <beakerboy99@yahoo.com>
2020-04-20 21:20:47 -0400

    [skip ci]

--- dev/travis/TestSuites/TestSuiteBase.php
+++ dev/travis/TestSuites/TestSuiteBase.php
@@ -26,4 +26,43 @@ abstract class TestSuiteBase extends TestSuite {
     return array_reduce($extension_directories, 'array_merge', []);
   }
 
+    /**
+   * Find and add tests to the suite for core and any extensions.
+   *
+   * @param string $root
+   *   Path to the root of the Drupal installation.
+   * @param string $suite_namespace
+   *   SubNamespace used to separate test suite. Examples: Unit, Functional.
+   * @param string $pattern
+   *   REGEXP pattern to apply to file name.
+   */
+  protected function addExtensionTestsBySuiteNamespace($root, $suite_namespace, $pattern) {
+    $failing_classes = [
+      'core/tests/Drupal/KernelTests/Core/Database/SelectSubqueryTest.php',
+      'core/tests/Drupal/KernelTests/Core/Database/SchemaTest.php',
+      'core/modules/dblog/tests/src/Kernel/Migrate/d6/MigrateDblogConfigsTest.php',
+      'core/modules/aggregator/tests/src/Kernel/Migrate/MigrateAggregatorStubTest.php',
+      'core/modules/comment/tests/src/Kernel/CommentIntegrationTest.php',
+    ];
+    // Extensions' tests will always be in the namespace
+    // Drupal\Tests\$extension_name\$suite_namespace\ and be in the
+    // $extension_path/tests/src/$suite_namespace directory. Not all extensions
+    // will have all kinds of tests.
+    foreach ($this->findExtensionDirectories($root) as $extension_name => $dir) {
+      if (preg_match("#^{$pattern}(.*)$#i", $extension_name) !== 0) {
+        $test_path = "$dir/tests/src/$suite_namespace";
+        if (is_dir($test_path)) {
+          $passing_tests = [];
+          $tests = TestDiscovery::scanDirectory("Drupal\\Tests\\$extension_name\\$suite_namespace\\", $test_path);
+          foreach ($tests as $test) {
+            if (!in_array($test, $failing_classes)) {
+              $passing_tests[] = $test;
+            }
+          }
+          $this->addTestFiles($passing_tests);
+        }
+      }
+    }
+  }
+
 }
