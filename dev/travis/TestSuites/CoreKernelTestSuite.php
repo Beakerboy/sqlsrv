d5f60ec70ca6f14890d2f55dbc80d2275e08789b
Kevin Nowaczyk <beakerboy99@yahoo.com>
2020-04-21 06:24:11 -0400

    Update CoreKernelTestSuite.php

--- dev/travis/TestSuites/CoreKernelTestSuite.php
+++ dev/travis/TestSuites/CoreKernelTestSuite.php
@@ -36,4 +36,18 @@ final class CoreKernelTestSuite extends TestSuiteBase {
     $this->addTestFiles(TestDiscovery::scanDirectory("Drupal\\KernelTests\\", "$root/core/tests/Drupal/KernelTests"));
   }
 
+  /**
+   * Find and add tests to the suite for core and any extensions.
+   *
+   * @param string $root
+   *   Path to the root of the Drupal installation.
+   * @param string $suite_namespace
+   *   SubNamespace used to separate test suite. Examples: Unit, Functional.
+   * @param string $pattern
+   *   REGEXP pattern to apply to file name.
+   */
+  protected function addExtensionTestsBySuiteNamespace($root, $suite_namespace, $pattern) {
+    // Do nothing.
+  }
+
 }
