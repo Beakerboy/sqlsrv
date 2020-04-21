7005741c7eb1e8b3d269b13fc67f67c22254771e
Kevin Nowaczyk <beakerboy99@yahoo.com>
2020-04-21 10:55:31 -0400

    Update CoreExtensions5KernelTestSuite.php

--- dev/travis/TestSuites/CoreExtensions5KernelTestSuite.php
+++ dev/travis/TestSuites/CoreExtensions5KernelTestSuite.php
@@ -24,29 +24,4 @@ final class CoreExtensions5KernelTestSuite extends TestSuiteBase {
     return $suite;
   }
 
-  /**
-   * Find and add tests to the suite for core and any extensions.
-   *
-   * @param string $root
-   *   Path to the root of the Drupal installation.
-   * @param string $suite_namespace
-   *   SubNamespace used to separate test suite. Examples: Unit, Functional.
-   * @param string $pattern
-   *   REGEXP pattern to apply to file name.
-   */
-  protected function addExtensionTestsBySuiteNamespace($root, $suite_namespace, $pattern) {
-    // Extensions' tests will always be in the namespace
-    // Drupal\Tests\$extension_name\$suite_namespace\ and be in the
-    // $extension_path/tests/src/$suite_namespace directory. Not all extensions
-    // will have all kinds of tests.
-    foreach ($this->findExtensionDirectories($root) as $extension_name => $dir) {
-      if (preg_match("#^{$pattern}(.*)$#i", $extension_name) !== 0) {
-        $test_path = "$dir/tests/src/$suite_namespace";
-        if (is_dir($test_path)) {
-          $this->addTestFiles(TestDiscovery::scanDirectory("Drupal\\Tests\\$extension_name\\$suite_namespace\\", $test_path));
-        }
-      }
-    }
-  }
-
 }
