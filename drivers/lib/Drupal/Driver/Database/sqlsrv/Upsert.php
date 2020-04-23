084d85c004b8ca82820c002b84caffd0becead9d
Kevin Nowaczyk <beakerboy99@yahoo.com>
2020-04-23 07:57:59 -0400

    [skip ci]

--- drivers/lib/Drupal/Driver/Database/sqlsrv/Upsert.php
+++ drivers/lib/Drupal/Driver/Database/sqlsrv/Upsert.php
@@ -19,7 +19,7 @@ class Upsert extends QueryUpsert {
     /** @var \Drupal\Driver\Database\sqlsrv\Schema $schema */
     $schema = $this->connection->schema();
     $blobFields = $schema->getBlobFields($this->table);
-    if (count($blobFields) === 0) {
+    if (count($blobFields) === 0 && $this->queryOptions['insecure'] === TRUE) {
       //  Emulate prepares.
       $this->queryOptions['insecure'] = TRUE;
       $this->queryOptions['allow_delimiter_in_query'] = TRUE;
