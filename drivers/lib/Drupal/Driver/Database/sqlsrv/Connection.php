fbd2b33baf922a0e003f2c5f78dc6689153f6d4c
Kevin Nowaczyk <beakerboy99@yahoo.com>
2020-04-27 07:32:39 -0400

    Update Connection.php

--- drivers/lib/Drupal/Driver/Database/sqlsrv/Connection.php
+++ drivers/lib/Drupal/Driver/Database/sqlsrv/Connection.php
@@ -319,8 +319,7 @@ class Connection extends DatabaseConnection {
     $options = $this->getConnectionOptions();
     $prefix = $this->tablePrefix($table);
     $schema_name = $this->schema->getDefaultSchema();
-    return parent:: getFullQualifiedTableName($table);
-    // return $options['database'] . '.' . $schema_name . '.' . $prefix . $table;
+    return $options['database'] . '.' . $schema_name . '.' . $prefix . $table;
   }
 
   /**
