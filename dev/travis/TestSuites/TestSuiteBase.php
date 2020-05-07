91021e5b4a19393c45c9488774ffa1690497af32
Kevin Nowaczyk <beakerboy99@yahoo.com>
2020-05-07 16:39:44 -0400

    Update TestSuiteBase.php

--- dev/travis/TestSuites/TestSuiteBase.php
+++ dev/travis/TestSuites/TestSuiteBase.php
@@ -27,21 +27,21 @@ abstract class TestSuiteBase extends TestSuite {
    * @var array
    */
   protected static $FunctionalExtensionPatterns = [
-    //'[a]',
-    //'[b]',
-   // '[c]',
-   // '[d]',
-    //'[e]',
-    '[f]',
-   // '[g-i]',
-    '[j-k]',
-    '[l]',
-    '[m-n]',
-    '[o-p]',
-    '[q-r]',
-    '[s-t]',
-    '[u-v]',
-    '[w-z]',
+    //'[a]', good
+    //'[b]', split
+   // '[c]', split
+   // '[d]', split
+    //'[e]', split
+    //'[f]', split
+   // '[g-i]', good
+    //'[j-k]', split
+    //'[l]', split
+    //'[m-n]', split
+    '[o]',
+    '[p]',
+    '[q]',
+    '[r]',
+    '[s]',
   ];
 
 
