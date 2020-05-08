598c71521970a203edf711cbc6968a7a6b65b329
Kevin Nowaczyk <beakerboy99@yahoo.com>
2020-05-08 11:31:12 -0400

    Update TestSuiteBase.php

--- dev/travis/TestSuites/TestSuiteBase.php
+++ dev/travis/TestSuites/TestSuiteBase.php
@@ -122,7 +122,7 @@ abstract class TestSuiteBase extends TestSuite {
             $passing_tests[] = $test;
           }
         }
-        $this->addTestFiles(array_splice($passing_tests, $splice * 20, 20));
+        $this->addTestFiles(array_splice($passing_tests, $splice * 10, 10));
       }
     }
   }
