0dce8d762e442f17ad4c8d11c72ade2a8ab29bda
Kevin Nowaczyk <beakerboy99@yahoo.com>
2020-05-08 09:26:18 -0400

    [skip ci]

--- dev/travis/TestSuites/TestSuiteBase.php
+++ dev/travis/TestSuites/TestSuiteBase.php
@@ -93,4 +93,38 @@ abstract class TestSuiteBase extends TestSuite {
     }
   }
 
+  /**
+   * Find and add tests to the suite for core and any extensions.
+   *
+   * @param string $root
+   *   Path to the root of the Drupal installation.
+   * @param string $suite_namespace
+   *   SubNamespace used to separate test suite. Examples: Unit, Functional.
+   * @param int $splice
+   *   The chunk number to test
+   */
+  protected function addExtensionTestsBySuiteNamespaceAndChunk($root, $suite_namespace, $splice = 0) {
+    $failing_classes = [];
+    foreach ($this->failingClasses as $failing_class) {
+      $failing_classes[] = $root . $failing_class;
+    }
+    // Extensions' tests will always be in the namespace
+    // Drupal\Tests\$extension_name\$suite_namespace\ and be in the
+    // $extension_path/tests/src/$suite_namespace directory. Not all extensions
+    // will have all kinds of tests.
+    foreach ($this->findExtensionDirectories($root) as $extension_name => $dir) {
+      $test_path = "$dir/tests/src/$suite_namespace";
+      if (is_dir($test_path)) {
+        $passing_tests = [];
+        $tests = TestDiscovery::scanDirectory("Drupal\\Tests\\$extension_name\\$suite_namespace\\", $test_path);
+        foreach ($tests as $test) {
+          if (!in_array($test, $failing_classes)) {
+            $passing_tests[] = $test;
+          }
+        }
+        $this->addTestFiles(array_splice($passing_tests, $splice * 100, 100));
+      }
+    }
+  }
+
 }
