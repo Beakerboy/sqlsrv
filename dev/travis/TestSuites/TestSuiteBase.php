86fb85fad046fe5fd76386aac1cb6adb41c1a999
Kevin Nowaczyk <beakerboy99@yahoo.com>
2020-05-13 18:20:37 -0400

    Update TestSuiteBase.php

--- dev/travis/TestSuites/TestSuiteBase.php
+++ dev/travis/TestSuites/TestSuiteBase.php
@@ -36,6 +36,7 @@ abstract class TestSuiteBase extends TestSuite {
     '/core/modules/node/tests/src/Kernel/Views/RevisionUidTest.php',
     '/core/modules/workspaces/tests/src/Kernel/WorkspaceIntegrationTest.php',
     '/core/modules/block_content/tests/src/Functional/Update/BlockContentReusableUpdateTest.php',
+    '/core/modules/block_content/tests/src/Functional/Update/BlockContentUpdateTest.php',
     '/core/modules/file/tests/src/Functional/Update/FileUpdateTest.php',
     '/core/modules/file/tests/src/Functional/Update/FileUsageTemporaryDeletionConfigurationUpdateTest.php',
     '/core/modules/hal/tests/src/Functional/Update/CreateHalSettingsForLinkDomainUpdateTest.php',
