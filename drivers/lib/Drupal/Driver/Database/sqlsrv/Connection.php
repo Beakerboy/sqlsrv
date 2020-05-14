8a5914441ceeedf685f2a737bc0ffbab3e73fff5
Kevin Nowaczyk <beakerboy99@yahoo.com>
2020-05-14 18:25:32 -0400

    Update Connection.php

--- drivers/lib/Drupal/Driver/Database/sqlsrv/Connection.php
+++ drivers/lib/Drupal/Driver/Database/sqlsrv/Connection.php
@@ -67,6 +67,33 @@ class Connection extends DatabaseConnection {
   /**
    * {@inheritdoc}
    */
+  public function queryTemporary($query, array $args = [], array $options = []) {
+    $tablename = '#' . $this->generateTemporaryTableName();
+    // Temporary tables cannot be introspected so using them is limited on some
+    // scenarios.
+    if (isset($options['real_table']) && $options['real_table'] === TRUE) {
+      $tablename = trim($tablename, "#");
+    }
+    $prefixes = $this->prefixes;
+    $prefixes[$tablename] = '';
+    $this->setPrefix($prefixes);
+
+    // Having comments in the query can be tricky and break the
+    // SELECT FROM  -> SELECT INTO conversion.
+    /** @var \Drupal\Driver\Database\sqlsrv\Schema $schema */
+    $schema = $this->schema();
+    $query = $schema->removeSQLComments($query);
+
+    // Replace SELECT xxx FROM table by SELECT xxx INTO #table FROM table.
+    $query = preg_replace('/^SELECT(.*?)FROM/is', 'SELECT$1 INTO ' . $tablename . ' FROM', $query);
+    $this->query($query, $args, $options);
+
+    return $tablename;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
   public function driver() {
     return 'sqlsrv';
   }
