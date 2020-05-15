16dc984fc24fbd8f1c44a7f134c32968553e310e
Kevin Nowaczyk <beakerboy99@yahoo.com>
2020-05-14 21:45:23 -0400

    Delete CONCAT.sql

--- drivers/lib/Drupal/Driver/Database/sqlsrv/Programability/ScalarValued/CONCAT.sql
+++
@@ -1,6 +0,0 @@
-CREATE FUNCTION [CONCAT](@op1 sql_variant, @op2 sql_variant) RETURNS nvarchar(4000) AS
-            BEGIN
-              DECLARE @result nvarchar(4000)
-              SET @result = CAST(@op1 AS nvarchar(4000)) + CAST(@op2 AS nvarchar(4000))
-              RETURN @result
-            END
