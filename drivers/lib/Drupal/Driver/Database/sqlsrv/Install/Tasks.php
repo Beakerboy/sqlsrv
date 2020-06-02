6de49da815a16ba06eea7c2e82548b57ee8dab75
Kevin Nowaczyk <beakerboy99@yahoo.com>
2020-06-02 09:14:41 -0400

    No default collation (#146)
    * Allow any CI collation as the default.
    * Update a couple core patches

--- drivers/lib/Drupal/Driver/Database/sqlsrv/Install/Tasks.php
+++ drivers/lib/Drupal/Driver/Database/sqlsrv/Install/Tasks.php
@@ -6,7 +6,6 @@ use Drupal\Core\Database\Database;
 use Drupal\Core\Database\Install\Tasks as InstallTasks;
 use Drupal\Core\Database\DatabaseNotFoundException;
 use Drupal\Driver\Database\sqlsrv\Connection;
-use Drupal\Driver\Database\sqlsrv\Schema;
 use Drupal\Driver\Database\sqlsrv\Utils;
 
 /**
@@ -126,13 +125,12 @@ class Tasks extends InstallTasks {
       /** @var \Drupal\Driver\Database\sqlsrv\Schema $schema */
       $schema = $database->schema();
       $collation = $schema->getCollation();
-      if ($collation == Schema::DEFAULT_COLLATION_CI || stristr($collation, '_UT') !== FALSE) {
-        $this->pass(t('Database is encoded in UTF8 collation: $collation'));
+      if (stristr($collation, '_CI_') !== FALSE) {
+        $this->pass(t('Database is encoded in case insensitive collation: $collation'));
       }
       else {
-        $this->fail(t('The %driver database is using %current collation, but must use UTF8 encoding (recomended %encoding) to work with Drupal. Recreate the database with %encoding encoding. See !link for more details.', [
+        $this->fail(t('The %driver database is using %current collation, but must use case insensitive collation to work with Drupal. Recreate the database with this collation. See !link for more details.', [
           '%current' => $collation,
-          '%encoding' => Schema::DEFAULT_COLLATION_CI,
           '%driver' => $this->name(),
           ':link' => '<a href="INSTALL.sqlsrv.txt">INSTALL.sqlsrv.txt</a>',
         ]));
