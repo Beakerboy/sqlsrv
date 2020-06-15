633c66d99ed1229dac0abe6eec34fe8c16039f61
Kevin Nowaczyk <beakerboy99@yahoo.com>
2020-06-15 12:42:56 -0400

    [skip ci]

--- dev/travis/TestSuites/CoreKernelTestSuite.php
+++ dev/travis/TestSuites/CoreKernelTestSuite.php
@@ -18,7 +18,7 @@ final class CoreKernelTestSuite extends TestSuiteBase {
    *   The test suite.
    */
   public static function suite() {
-    $root = dirname(__DIR__, 6);
+    $root = $this->getDrupalRoot();
     $suite = new static('kernel');
     $suite->addCoreKernelTests($root);
     return $suite;
