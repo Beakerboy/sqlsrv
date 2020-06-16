0f238ab096066e7ff50112840816f47d7ba897e9
Kevin Nowaczyk <beakerboy99@yahoo.com>
2020-06-16 17:11:54 -0400

    [skip ci]

---
+++ tests/database_statement_monitoring_test/Install/Tasks.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Drupal\database_statement_monitoring_test\sqlsrv\Install;
+
+use Drupal\sqlsrv\Database\Driver\sqlsrv\Install\Tasks as BaseTasks;
+
+class Tasks extends BaseTasks {
+}
