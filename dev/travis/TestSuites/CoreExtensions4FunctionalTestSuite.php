0acbd1869cc415958911d6d2a023de93e33bbf04
Kevin Nowaczyk <beakerboy99@yahoo.com>
2020-06-17 06:38:32 -0400

    Update CoreExtensions4FunctionalTestSuite.php

--- dev/travis/TestSuites/CoreExtensions4FunctionalTestSuite.php
+++ dev/travis/TestSuites/CoreExtensions4FunctionalTestSuite.php
@@ -16,7 +16,7 @@ final class CoreExtensions4FunctionalTestSuite extends TestSuiteBase {
    *   The test suite.
    */
   public static function suite() {
-    $root = $this->getDrupalRoot();
+    $root = self::getDrupalRoot();
     $suite = new static('functional');
     $suite->addExtensionTestsBySuiteNamespaceAndChunk($root, 'Functional', 3);
     return $suite;
