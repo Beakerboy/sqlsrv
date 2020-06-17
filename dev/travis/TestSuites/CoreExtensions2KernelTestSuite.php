11cd90b00c7d836bdc39824fe97778409f388c73
Kevin Nowaczyk <beakerboy99@yahoo.com>
2020-06-17 06:17:43 -0400

    [skip ci]

--- dev/travis/TestSuites/CoreExtensions2KernelTestSuite.php
+++ dev/travis/TestSuites/CoreExtensions2KernelTestSuite.php
@@ -16,7 +16,7 @@ final class CoreExtensions2KernelTestSuite extends TestSuiteBase {
    *   The test suite.
    */
   public static function suite() {
-    $root = $this->getDrupalRoot();
+    $root = self::getDrupalRoot();
     $suite = new static('kernel');
     $suite->addExtensionTestsBySuiteNamespace($root, 'Kernel', self::$coreExtensionPatterns[1]);
     return $suite;
