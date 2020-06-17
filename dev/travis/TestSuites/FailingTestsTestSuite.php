8ba2da3ea9fc598bcbc5ae894a9f167bcfe982a4
Kevin Nowaczyk <beakerboy99@yahoo.com>
2020-06-17 06:39:40 -0400

    Update FailingTestsTestSuite.php

--- dev/travis/TestSuites/FailingTestsTestSuite.php
+++ dev/travis/TestSuites/FailingTestsTestSuite.php
@@ -16,7 +16,7 @@ final class FailingTestsTestSuite extends TestSuiteBase {
    *   The test suite.
    */
   public static function suite() {
-    $root = $this->getDrupalRoot();
+    $root = self::getDrupalRoot();
     $suite = new static('kernel');
     $suite->addFailingTests($root);
     return $suite;
