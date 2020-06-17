37506d8d821e068467f9ea31d8d4c46367d31a2b
Kevin Nowaczyk <beakerboy99@yahoo.com>
2020-06-17 06:27:23 -0400

    Update CoreExtensions3KernelTestSuite.php

--- dev/travis/TestSuites/CoreExtensions3KernelTestSuite.php
+++ dev/travis/TestSuites/CoreExtensions3KernelTestSuite.php
@@ -16,7 +16,7 @@ final class CoreExtensions3KernelTestSuite extends TestSuiteBase {
    *   The test suite.
    */
   public static function suite() {
-    $root = $this->getDrupalRoot();
+    $root = self::getDrupalRoot();
     $suite = new static('kernel');
     $suite->addExtensionTestsBySuiteNamespace($root, 'Kernel', self::$coreExtensionPatterns[2]);
     return $suite;
