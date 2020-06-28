f719cc88aded2fd8f40a1dd233f73e3ce5c33731
Kevin Nowaczyk <beakerboy99@yahoo.com>
2020-06-27 20:37:32 -0400

    Update drupal9.0.x.sh

--- dev/travis/drupal9.0.x.sh
+++ dev/travis/drupal9.0.x.sh
@@ -1,6 +1,9 @@
 # encapsulate fields.
 wget http://beakerboy.com/~kevin/3136974-4.patch
 git apply 3136974-4.patch
+# ConnectionUnitTest defaults to MySQL syntax
+wget http://beakerboy.com/~kevin/connectionUnit.patch
+git apply connectionUnit.patch
 # core Condition not able to be overridden in views...needs work.
 wget https://www.drupal.org/files/issues/2020-05-04/3130655-13.patch
 git apply 3130655-13.patch
