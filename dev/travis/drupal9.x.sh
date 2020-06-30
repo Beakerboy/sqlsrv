82e99c565409957655dbe4077c3864536b3f8ff8
Kevin Nowaczyk <beakerboy99@yahoo.com>
2020-06-30 08:15:40 -0400

    Remove bootstrap patch

--- dev/travis/drupal9.x.sh
+++ dev/travis/drupal9.x.sh
@@ -5,9 +5,6 @@ git apply 3136974-4.patch
 # Core Condition not able to be overridden in views...needs work.
 wget https://www.drupal.org/files/issues/2020-05-04/3130655-13.patch
 git apply 3130655-27.patch
-# Include bootstrap.inc by composer
-wget https://www.drupal.org/files/issues/2020-06-21/3151118-25.patch
-git apply 3151118-25.patch
 
 # Testing Patches
 # ConnectionUnitTest defaults to MySQL syntax
@@ -19,3 +16,6 @@ git apply 2966272-16.patch
 # Sort order must be specified
 wget https://www.drupal.org/files/issues/2020-06-12/3146016-5.patch
 git apply 3146016-5.patch
+# include bootstrap earlier
+https://www.drupal.org/files/issues/2020-06-16/3152003-2.patch
+git apply 3152003-2.patch
