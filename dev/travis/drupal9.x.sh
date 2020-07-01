5faa0084497130bee2b11a67d54f782f954aa242
Kevin Nowaczyk <beakerboy99@yahoo.com>
2020-07-01 09:34:26 -0400

    Update drupal9.x.sh

--- dev/travis/drupal9.x.sh
+++ dev/travis/drupal9.x.sh
@@ -3,7 +3,7 @@
 wget http://beakerboy.com/~kevin/3136974-4.patch
 git apply 3136974-4.patch
 # Core Condition not able to be overridden in views...needs work.
-wget https://www.drupal.org/files/issues/2020-05-04/3130655-13.patch
+wget https://www.drupal.org/files/issues/2020-05-04/3130655-27.patch
 git apply 3130655-27.patch
 
 # Testing Patches
