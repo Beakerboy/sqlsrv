d610ba2862e3c53a634dedd345c6658657b1bfcd
Kevin Nowaczyk <beakerboy99@yahoo.com>
2020-07-02 09:42:55 -0400

    Update drupal9.x.sh

--- dev/travis/drupal9.x.sh
+++ dev/travis/drupal9.x.sh
@@ -22,3 +22,13 @@ git apply 3146016-5.patch
 # Add a sqlsrv-specific datatype to test
 wget https://www.drupal.org/files/issues/2020-02-05/drupal-3111134-database_specific_types-3.patch
 git apply drupal-3111134-database_specific_types-3.patch
+
+# Reorganize files and directories.
+mv $TRAVIS_BUILD_DIR/dev/travis/TestSuites $TRAVIS_BUILD_DIR/tests/src/
+rm -rf $TRAVIS_BUILD_DIR/dev/appveyor
+mv $TRAVIS_BUILD_DIR/tests/database_statement_monitoring_test ./core/modules/system/tests/modules/database_statement_monitoring_test/src/sqlsrv
+cp -rf $TRAVIS_BUILD_DIR ./modules
+PATH=$PATH:$TRAVIS_BUILD_DIR/../drupal-project/vendor/bin
+mkdir $TRAVIS_BUILD_DIR/../drupal-project/sites/simpletest
+mkdir $TRAVIS_BUILD_DIR/../drupal-project/sites/simpletest/browser_output
+sed -e "s?WEB_DIR?$(pwd)?g" --in-place $TRAVIS_BUILD_DIR/dev/travis/phpunit.xml.dist
