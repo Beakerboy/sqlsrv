7c87589943ec2512325bda8736b61e25261785c5
Kevin Nowaczyk <beakerboy99@yahoo.com>
2020-07-26 11:15:42 -0400

    Update mssql2016.ps1

--- dev/appveyor/mssql2016.ps1
+++ dev/appveyor/mssql2016.ps1
@@ -1,4 +1,1 @@
-
-Invoke-Sqlcmd -Database mydrupalsite -Username sa -Password "Password12!" -Query "EXEC sp_configure 'show advanced options', 1; RECONFIGURE; EXEC sp_configure 'clr enable', 1; RECONFIGURE"
-Invoke-Sqlcmd -Database mydrupalsite -Username sa -Password "Password12!" -Query "CREATE ASSEMBLY Regex from 'C:\testlogs\RegEx.dll' WITH PERMISSION_SET = SAFE"
-Invoke-Sqlcmd -Database mydrupalsite -Username sa -Password "Password12!" -Query "CREATE FUNCTION dbo.REGEXP(@pattern NVARCHAR(100), @matchString NVARCHAR(100)) RETURNS bit EXTERNAL NAME Regex.RegExCompiled.RegExCompiledMatch"
+Invoke-Sqlcmd -Query "CREATE DATABASE mydrupalsite" -ConnectionString "(local)\SQL2016;Database=master;User ID=sa;Password=Password12!"
