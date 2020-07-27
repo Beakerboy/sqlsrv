36ec64a3bbc53f76824a71a426a883129418aa35
Kevin Nowaczyk <beakerboy99@yahoo.com>
2020-07-27 14:24:44 -0400

    [skip ci]

---
+++ dev/appveyor/TestSuites/AppveyorTestSuiteBase.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Drupal\Tests\sqlsrv\TestSuites;
+
+/**
+ * Base class for Drupal test suites.
+ */
+abstract class AppveyorTestSuiteBase extends TestSuiteBase {
+
+  /**
+   * Regex patterns to split up core Kernel extensions.
+   *
+   * @var array
+   */
+  protected static $coreExtensionPatterns = [
+    '[a-g]',
+    '[h-q]',
+    '[r-z]',
+  ];
+
+ }
