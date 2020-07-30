38dbe5de67262b971f6484d6f0b20a21864db0bc
Kevin Nowaczyk <beakerboy99@yahoo.com>
2020-07-30 06:33:09 -0400

    Update CITestSuiteBase.php

--- dev/appveyor/CITestSuiteBase.php
+++ dev/appveyor/CITestSuiteBase.php
@@ -8,6 +8,15 @@ require_once __DIR__ . '/TestSuiteBase.php';
  * Base class for Drupal test suites.
  */
 abstract class CITestSuiteBase extends TestSuiteBase {
+  /**
+   * Regex patterns to split up core Kernel tests.
+   *
+   * @var array
+   */
+  protected static $corePatterns = [
+    '[a-m]',
+    '[n-z]',
+  ];
 
   /**
    * Regex patterns to split up core Kernel extensions.
