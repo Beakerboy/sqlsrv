e2f0b63fff6bc9c84db9f6223d91c9f30d63ec3b
Kevin Nowaczyk <beakerboy99@yahoo.com>
2020-07-30 15:46:30 -0400

    [skip ci]

--- dev/TestSuites/TestSuiteBase.php
+++ dev/TestSuites/TestSuiteBase.php
@@ -50,6 +50,28 @@ abstract class TestSuiteBase extends TestSuite {
     return array_reduce($extension_directories, 'array_merge', []);
   }
 
+  /**
+   * Find and add tests to the suite for core and any extensions.
+   *
+   * @param string $root
+   *   Path to the root of the Drupal installation.
+   */
+  protected function addCoreKernelTestsByName($root) {
+    $failing_classes = [];
+    foreach ($this->failingClasses as $failing_class) {
+      $failing_classes[] = $root . $failing_class;
+    }
+    // Core's Kernel tests are in the namespace Drupal\KernelTests\ and are
+    // always inside of core/tests/Drupal/KernelTests.
+    $passing_tests = [];
+    $tests = TestDiscovery::scanDirectory("Drupal\\KernelTests\\", "$root/core/tests/Drupal/KernelTests");
+    foreach ($tests as $test) {
+      if (!in_array($test, $failing_classes) && preg_match("#.*\/{$pattern}[a-zA-z]*\.php$#i", $test) !== 0) {
+        $passing_tests[] = $test;
+      }
+    }
+    $this->addTestFiles($passing_tests);
+  }
   
   /**
    * Find and add tests to the suite for core and any extensions.
