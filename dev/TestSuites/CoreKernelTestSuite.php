18079eee5c841202e0051ce7f3ff80634a38f3fe
Kevin Nowaczyk <beakerboy99@yahoo.com>
2020-07-30 15:31:51 -0400

    [skip ci]

--- dev/TestSuites/CoreKernelTestSuite.php
+++ dev/TestSuites/CoreKernelTestSuite.php
@@ -20,31 +20,8 @@ final class CoreKernelTestSuite extends TestSuiteBase {
   public static function suite() {
     $root = self::getDrupalRoot();
     $suite = new static('kernel');
-    $suite->addCoreKernelTests($root);
+    $suite->addCoreKernelTestsByNamespace($root, self::$coreKernelPatterns[0]);
     return $suite;
-  }
-
-  /**
-   * Find and add tests to the suite for core and any extensions.
-   *
-   * @param string $root
-   *   Path to the root of the Drupal installation.
-   */
-  protected function addCoreKernelTests($root) {
-    $failing_classes = [];
-    foreach ($this->failingClasses as $failing_class) {
-      $failing_classes[] = $root . $failing_class;
-    }
-    // Core's Kernel tests are in the namespace Drupal\KernelTests\ and are
-    // always inside of core/tests/Drupal/KernelTests.
-    $passing_tests = [];
-    $tests = TestDiscovery::scanDirectory("Drupal\\KernelTests\\", "$root/core/tests/Drupal/KernelTests");
-    foreach ($tests as $test) {
-      if (!in_array($test, $failing_classes)) {
-        $passing_tests[] = $test;
-      }
-    }
-    $this->addTestFiles($passing_tests);
   }
 
 }
