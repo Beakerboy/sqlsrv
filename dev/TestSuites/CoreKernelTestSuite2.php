542f39e9e0e432c403c651f2ebaebc0992ed8ae9
Kevin Nowaczyk <beakerboy99@yahoo.com>
2020-07-30 21:24:03 -0400

    [skip ci]

---
+++ dev/TestSuites/CoreKernelTestSuite2.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Drupal\Tests\sqlsrv\TestSuites;
+
+use Drupal\Core\Test\TestDiscovery;
+
+require_once __DIR__ . '/CITestSuiteBase.php';
+
+/**
+ * Discovers tests for the kernel test suite.
+ */
+final class CoreKernelTestSuite2 extends CITestSuiteBase {
+
+  /**
+   * Factory method which loads up a suite with all core kernel tests.
+   *
+   * @return static
+   *   The test suite.
+   */
+  public static function suite() {
+    $root = self::getDrupalRoot();
+    $suite = new static('kernel');
+    $suite->addCoreKernelTestsByName($root, self::$coreKernelPatterns[1]);
+    return $suite;
+  }
+
+}
