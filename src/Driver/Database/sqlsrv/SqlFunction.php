0ab75255eb0f9885d0d13b97a1392ae704aeb6d1
Kevin Nowaczyk <beakerboy99@yahoo.com>
2020-08-06 08:27:36 -0400

    [skip ci]

--- src/Driver/Database/sqlsrv/SqlFunction.php
+++ src/Driver/Database/sqlsrv/SqlFunction.php
@@ -35,6 +35,8 @@ class SqlFunction {
     * Return the expression as a string in SQL Server syntax.
     */
    public compile() {
+     
+     return $this->functionName . '(' . $arguments . ')';
    }
  
    /**
@@ -68,5 +70,36 @@ class SqlFunction {
     }
     return FALSE;
   }
-  
+
+  /**
+   * CONCAT_WS.
+   *
+   * CONCAT_WS(sep, arg1, arg2, arg3, ...)
+   * Becomes
+   * STUFF(COALESCE(sep + arg1, '') + COALESCE(sep + arg2, '') + COALESCE(sep+arg3, '') + ..., 1, len(sep), '')
+   */
+  function concat_ws() {
+    if (($pos1 = stripos($snippet, 'CONCAT_WS(')) !== FALSE) {
+      // We assume the the separator does not contain any single-quotes
+      // and none of the arguments contain commas.
+      $pos2 = $this->findParenMatch($snippet, $pos1 + 9);
+      $argument_list = substr($snippet, $pos1 + 10, $pos2 - 10 - $pos1);
+      $arguments = explode(', ', $argument_list);
+      $closing_quote_pos = stripos($argument_list, '\'', 1);
+      $separator = substr($argument_list, 1, $closing_quote_pos - 1);
+      $strings_list = substr($argument_list, $closing_quote_pos + 3);
+      $arguments = explode(', ', $strings_list);
+      $replace = "STUFF(";
+      $coalesce = [];
+      foreach ($arguments as $argument) {
+        $coalesce[] = "COALESCE('{$separator}' + {$argument}, '')";
+      }
+      $coalesce_string = implode(' + ', $coalesce);
+      $sep_len = strlen($separator);
+      $replace = "STUFF({$coalesce_string}, 1, {$sep_len}, '')";
+      $snippet = substr($snippet, 0, $pos1) . $replace . substr($snippet, $pos2 + 1);
+      $operator = NULL;
+    }
+  }
+
 }
