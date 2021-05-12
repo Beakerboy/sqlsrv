92244558b3baa77da34ea428c0ced1c0a8cfa8ff
Kevin Nowaczyk <beakerboy99@yahoo.com>
2021-05-12 09:26:00 -0400

    Update StatementWrapperLegacyTest.php

--- tests/src/Kernel/StatementWrapperLegacyTest.php
+++ tests/src/Kernel/StatementWrapperLegacyTest.php
@@ -20,7 +20,7 @@ class StatementWrapperLegacyTest extends DatabaseTestBase {
    */
   protected function setUp(): void {
     parent::setUp();
-    $this->statement = $this->connection->prepareStatement('SELECT * FROM {test}', []);
+    $this->statement = $this->connection->prepareStatement('SELECT id FROM {test}', []);
     if (!$this->statement instanceof StatementWrapper) {
       $this->markTestSkipped('This test only works for drivers implementing Drupal\Core\Database\StatementWrapper.');
     }
@@ -30,6 +30,8 @@ class StatementWrapperLegacyTest extends DatabaseTestBase {
    * Tests calling a non existing \PDOStatement method.
    */
   public function testMissingMethod() {
+    $class = $this->statement::class;
+    $this->assertEquals($class, "Foo");
     $PDO = new \PDOStatement();
     $this->assertFalse(is_callable([$PDO, 'boo']));
   }
@@ -42,6 +44,6 @@ class StatementWrapperLegacyTest extends DatabaseTestBase {
     $this->statement->execute();
     $array = $this->statement->fetch(\PDO::FETCH_ASSOC);
     $this->assertEquals($array, array("1", "Foo"));
-    $this->assertEquals(4, $this->statement->columnCount());
+    $this->assertEquals(1, $this->statement->columnCount());
   }
 }
