2b1ca9e2331bdb1ea6297944582cb2ce0dc5c7b4
Kevin Nowaczyk <beakerboy99@yahoo.com>
2021-05-19 10:23:10 -0400

    Update drupal7.x.sh

--- dev/travis/drupal7.x.sh
+++ dev/travis/drupal7.x.sh
@@ -1,7 +1,7 @@
 rm ../drupal-project/scripts/run-tests.sh
 rm ../drupal-project/includes/bootstrap.inc
 cp -R $TRAVIS_BUILD_DIR ../drupal-project/sites/all/modules/
-cp -R $TRAVIS_BUILD_DIR/sqlsrv ../drupal-project/includes/databases/
+cp -R $TRAVIS_BUILD_DIR/sqlsrv ../drupal-project/includes/database/
 cp $TRAVIS_BUILD_DIR/dev/travis/bootstrap.inc ../drupal-project/includes/
 cp $TRAVIS_BUILD_DIR/dev/travis/run-tests.sh ../drupal-project/scripts/
 cp $TRAVIS_BUILD_DIR/dev/settings.php ../drupal-project/sites/default/
