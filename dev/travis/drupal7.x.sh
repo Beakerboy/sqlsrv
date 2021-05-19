766f2e84b7dbfa94797f5a7b20385286caf9f50e
Kevin Nowaczyk <beakerboy99@yahoo.com>
2021-05-19 13:30:32 -0400

    Update drupal7.x.sh

--- dev/travis/drupal7.x.sh
+++ dev/travis/drupal7.x.sh
@@ -1,3 +1,3 @@
-cp -R $TRAVIS_BUILD_DIR ../drupal-project/sites/all/modules/
-cp -R $TRAVIS_BUILD_DIR/sqlsrv ../drupal-project/includes/database/
-cp $TRAVIS_BUILD_DIR/dev/settings.php ../drupal-project/sites/default/
+cp -R $TRAVIS_BUILD_DIR $TRAVIS_BUILD_DIR/../drupal-project/sites/all/modules/
+cp -R $TRAVIS_BUILD_DIR/sqlsrv $TRAVIS_BUILD_DIR/../drupal-project/includes/database/
+cp $TRAVIS_BUILD_DIR/dev/settings.php $TRAVIS_BUILD_DIR/../drupal-project/sites/default/
