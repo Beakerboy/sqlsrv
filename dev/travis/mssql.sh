7a04fb3dcdf7125773ce0fedf6cd35ed82050e89
Kevin Nowaczyk <beakerboy99@yahoo.com>
2021-05-19 11:53:42 -0400

    Update mssql.sh

--- dev/travis/mssql.sh
+++ dev/travis/mssql.sh
@@ -13,7 +13,10 @@ sleep 15
 /opt/mssql-tools/bin/sqlcmd -P Password12! -S localhost -U SA -Q "CREATE DATABASE mydrupalsite COLLATE ${DB_COLLATION}"
 # Install the pdo_sqlsrv extension
 sudo ACCEPT_EULA=Y apt-get -y install msodbcsql17 unixodbc-dev gcc g++ make autoconf libc-dev pkg-config
-pecl install sqlsrv-5.6.1 pdo_sqlsrv-5.6.1
+# PHP7.1
+# pecl install sqlsrv-5.6.1 pdo_sqlsrv-5.6.1
+# PHP7.0
+pecl install sqlsrv-5.3.0 pdo_sqlsrv-5.3.0
 yes "autodetect" | pecl install yaml
 # Install REGEX CLR
 wget https://github.com/Beakerboy/drupal-sqlsrv-regex/releases/download/1.0/RegEx.dll
