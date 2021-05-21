9fe548873ffbafd183704aad5a956636ab4db4ca
Kevin Nowaczyk <beakerboy99@yahoo.com>
2021-05-21 06:17:14 -0400

    [skip travis]

--- dev/appveyor/settings.php
+++ dev/appveyor/settings.php
@@ -311,7 +311,7 @@ $drupal_hash_salt = '';
  * It is not allowed to have a trailing slash; Drupal will add it
  * for you.
  */
-# $base_url = 'http://www.example.com';  // NO trailing slash!
+ $base_url = 'http://127.0.0.1';  // NO trailing slash!
 /**
  * PHP settings:
  *
